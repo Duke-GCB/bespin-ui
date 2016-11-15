7bb3e277873f1e8c96a84106473c3abafc394b49
Dan Leehr <dan.leehr@duke.edu>
2016-11-15 10:10:12 -0500

    Uses article tags for project, adds test

--- tests/integration/components/projects-list-test.js
+++ tests/integration/components/projects-list-test.js
@@ -9,16 +9,10 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{projects-list}}`);
+  this.set('data', [{id: 1, name:'one'}, {id:2,name:'two'}]);
+  this.render(hbs`{{projects-list data}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$('article').length, 2);
+  assert.equal(this.$('article').first().text(), '1 - one');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#projects-list}}
-      template block text
-    {{/projects-list}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
