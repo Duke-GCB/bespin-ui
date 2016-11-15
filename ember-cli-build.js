1f3b1838c5ef03c92dc0fb6e0625db45744266ba
Dan Leehr <dan.leehr@duke.edu>
2016-11-15 13:40:20 -0500

    Disable theme in ember-cli-build
    Bootstrap theme adds gradient to buttons and other controls. Keeping these off for now.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
     'ember-bootstrap': {
-      'importBootstrapTheme': true
+      'importBootstrapTheme': false
     }
   });
 
