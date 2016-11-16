87fae1f2b7ba031ef0a3dcd82d46fe36a363527b
Dan Leehr <dan.leehr@duke.edu>
2016-11-15 21:46:16 -0500

    Adds semicolons to factories/project.js

--- mirage/factories/project.js
+++ mirage/factories/project.js
@@ -1,7 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  uuid() { return faker.random.uuid()},
-  name() { return faker.company.catchPhrase()},
-  description() { return faker.lorem.paragraph() }
+  uuid() { return faker.random.uuid(); },
+  name() { return faker.company.catchPhrase(); },
+  description() { return faker.lorem.paragraph(); }
 });
