b55b6b2b289b86b82029e28a85f589600ba3f46f
Dan Leehr <dan.leehr@duke.edu>
2016-11-16 18:01:33 -0500

    Tried to write some controller tests for login, but they're not very useful
    controllers are meant to be unit tests and I'm trying to write tests that have async/promise behavior.

--- tests/unit/controllers/login-test.js
+++ tests/unit/controllers/login-test.js
@@ -1,12 +1,28 @@
 import { moduleFor, test } from 'ember-qunit';
 
+const sessionStub = Ember.Service.extend({
+  authenticate() {
+    return new Ember.RSVP.Promise((resolve, reject) => { resolve(); });
+  }
+});
+
 moduleFor('controller:login', 'Unit | Controller | login', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  beforeEach: function() {
+    this.register('service:session', sessionStub);
+    this.inject.service('session', {as: 'session'});
+  }
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
   let controller = this.subject();
   assert.ok(controller);
+});
+
+test('it handles authenticate', function(assert) {
+  const controller = this.subject();
+  controller.set('identification', 'user123');
+  controller.set('password', 'secret');
+  controller.send('authenticate');
+  assert.notOk(controller.get('errorMessage'));
 });
