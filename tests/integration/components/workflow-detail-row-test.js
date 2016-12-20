40cfd6a5f6036991e0d3ae19f5681fd240512846
Dan Leehr <dan.leehr@duke.edu>
2016-12-20 13:56:26 -0500

    Integratino tests for workflow detail row

--- tests/integration/components/workflow-detail-row-test.js
+++ tests/integration/components/workflow-detail-row-test.js
@@ -5,20 +5,29 @@ moduleForComponent('workflow-detail-row', 'Integration | Component | workflow de
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders workflow details', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('workflow', {name: 'Workflow 123', description: 'Sample workflow', selected: false});
+  this.render(hbs`{{workflow-detail-row workflow}}`);
+  assert.equal(this.$('.workflow-name').text().trim(), 'Workflow 123');
+  assert.equal(this.$('.workflow-description').text().trim(), 'Sample workflow');
+});
 
-  this.render(hbs`{{workflow-detail-row}}`);
+test('it renders workflow selected state', function(assert) {
+  this.set('workflow', {name:'Workflow 123', description: 'Sample workflow', selected: false});
+  this.render(hbs`{{workflow-detail-row workflow}}`);
+  assert.equal(this.$('.glyphicon-ok').length, 0);
 
-  assert.equal(this.$().text().trim(), '');
+  this.set('workflow', {name:'Workflow 123', description: 'Sample workflow', selected: true});
+  this.render(hbs`{{workflow-detail-row workflow}}`);
+  assert.equal(this.$('.glyphicon-ok').length, 1);
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#workflow-detail-row}}
-      template block text
-    {{/workflow-detail-row}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it renders workflow versions', function(assert) {
+  // Test it renders workflow versions
+  let versions = [{'version': 1}, {'version': 2}];
+  this.set('workflow', {name:'Workflow 123', description: 'Sample workflow with 2 versions', selected: true, versions: versions});
+  this.render(hbs`{{workflow-detail-row workflow}}`);
+  assert.equal(this.$('.workflow-detail-version-row').length, 2);
 });
