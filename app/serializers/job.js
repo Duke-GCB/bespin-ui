88ae6515e76504e79999bfe63cc1bbdce049ad32
Dan Leehr <dan.leehr@duke.edu>
2016-12-20 14:06:19 -0500

    Remove unused argument from job serializer

--- app/serializers/job.js
+++ app/serializers/job.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.JSONAPISerializer.extend({
-  keyForRelationship: function(key, relationship) {
+  keyForRelationship: function(key /*, relationship */) {
     let dasherized = Ember.String.underscore(key);
     console.log(`key: ${key} -> path: ${dasherized}`);
     return dasherized;
