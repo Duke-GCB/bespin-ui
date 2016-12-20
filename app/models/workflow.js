4bf6152e2497069685d67776de05fca789d5a44f
Dan Leehr <dan.leehr@duke.edu>
2016-12-20 15:18:52 -0500

    Add description attribute to workflow (not yet implemented on server

--- app/models/workflow.js
+++ app/models/workflow.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  versions: DS.hasMany('workflow-version')
+  versions: DS.hasMany('workflow-version'),
+  description: DS.attr('string')
 });
