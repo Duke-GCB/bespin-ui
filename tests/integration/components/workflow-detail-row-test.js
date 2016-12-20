b895f9dfd79e277a894e05a22c0e5121a664896d
Dan Leehr <dan.leehr@duke.edu>
2016-12-20 15:39:05 -0500

    Reorganize workflow/workflow-version templates

--- tests/integration/components/workflow-detail-row-test.js
+++ tests/integration/components/workflow-detail-row-test.js
@@ -24,10 +24,14 @@ test('it renders workflow selected state', function(assert) {
   assert.equal(this.$('.glyphicon-ok').length, 1);
 });
 
-test('it renders workflow versions', function(assert) {
-  // Test it renders workflow versions
-  let versions = [{'version': 1}, {'version': 2}];
-  this.set('workflow', {name:'Workflow 123', description: 'Sample workflow with 2 versions', selected: true, versions: versions});
-  this.render(hbs`{{workflow-detail-row workflow}}`);
-  assert.equal(this.$('.workflow-detail-version-row').length, 2);
+test('it renders block content', function(assert) {
+  this.set('workflow', {name:'Workflow 123', description: 'Sample workflow'});
+// Template block usage:
+  this.render(hbs`
+    {{#workflow-detail-row workflow}}
+      Block Content
+    {{/workflow-detail-row}}
+  `);
+
+  assert.equal(this.$('.workflow-content').text().trim(), 'Block Content');
 });
--- tests/integration/components/workflow-versions-list-test.js
+++ tests/integration/components/workflow-versions-list-test.js
@@ -1,19 +1,15 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('workflows-list', 'Integration | Component | workflows list', {
+moduleForComponent('workflow-versions-list', 'Integration | Component | workflow versions list', {
   integration: true
 });
 
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-
   let versions = [{'version': 1}, {'version': 2}];
-  let workflow = {name: 'Workflow 123', description: 'Sample workflow', selected: false, versions: versions};
-  this.set('workflows', [workflow]);
-
-  this.render(hbs`{{workflows-list workflows}}`);
-  assert.equal(this.$('.workflow-detail-row').length, 1);
+  this.set('versions', versions);
+  this.render(hbs`{{workflow-versions-list versions}}`);
   assert.equal(this.$('.workflow-version-detail-row').length, 2);
 });
