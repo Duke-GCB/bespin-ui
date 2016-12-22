57e485549fb2d97bde57fd39c418f346e6774f9b
Dan Leehr <dan.leehr@duke.edu>
2016-12-22 15:38:35 -0500

    Some more fields on creating a job
    - This now creates a job thorough the JSON-API adapter using the json-resource-name branch of bespin-api.
    - But it required changing the serializer base class on the other end

--- app/components/jobs/new-job.js
+++ app/components/jobs/new-job.js
@@ -1,12 +1,25 @@
 import Ember from 'ember';
 
 const NewJob = Ember.Component.extend({
+  job: null,
+  errors: [],
+  workflows: [],
   actions: {
     workflowChanged(workflow) {
       console.log('workflow changed to ' + workflow.get('name'));
       console.log('setting job workflow version to ' +  workflow.get('latestVersion').get('url'));
       console.log('this job is' + this.get('job').get('name'));
       this.get('job').set('workflowVersion', workflow.get('latestVersion'));
+    },
+    create() {
+      console.log('create clicked!');
+      let job = this.get('job');
+      job.save().then(() => {
+        //ok
+      }, () => {
+        // error
+        this.set('errors', job.get('errors'));
+      });
     }
   }
 });
