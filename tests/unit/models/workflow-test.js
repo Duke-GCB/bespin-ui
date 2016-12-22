ce82fc7d991486aa2302111581a5fd08be7aa184
Dan Leehr <dan.leehr@duke.edu>
2016-12-22 09:40:57 -0500

    Test for computing latest version

--- tests/unit/models/workflow-test.js
+++ tests/unit/models/workflow-test.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 moduleForModel('workflow', 'Unit | Model | workflow', {
   // Specify the other units that are required for this test.
-  needs: ['model:workflow-version']
+  needs: ['model:workflow-version', 'model:job']
 });
 
 test('it exists', function(assert) {
@@ -18,4 +18,24 @@ test('it has many workflow-versions', function(assert) {
   assert.equal(relationship.key, 'versions', 'has relationship with workflow-version');
   assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
   assert.equal(relationship.type, 'workflow-version', 'Type of related object is workflow-version');
+});
+
+test('it computes latest version', function(assert) {
+  let workflow = this.subject();
+  Ember.run(() => {
+    this.store().createRecord('workflow-version', {
+      version: 1,
+      workflow:workflow
+    });
+  });
+  assert.equal(workflow.get('latestVersion').get('version'), 1);
+  assert.equal(workflow.get('versions').get('length'), 1);
+  Ember.run(() => {
+    this.store().createRecord('workflow-version', {
+      version: 2,
+      workflow:workflow
+    });
+  });
+  assert.equal(workflow.get('latestVersion').get('version'), 2);
+  assert.equal(workflow.get('versions').get('length'), 2);
 });
