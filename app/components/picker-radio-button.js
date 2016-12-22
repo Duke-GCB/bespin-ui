6791f697418b0783af8d861dea01c3b81fbae64c
Dan Leehr <dan.leehr@duke.edu>
2016-12-22 10:34:49 -0500

    Workflow picker with radio buttons
    - Reworks workflow picker to use a radio button
    - Panel title is a label for the radio button, using concatenation of
    component id for a radio button id
    - includes ember-radio-button (extended for sending change events up)

---
+++ app/components/picker-radio-button.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import EmberRadioButton from 'ember-radio-buttons';
+
+export default EmberRadioButton.extend({
+  change: function() {
+    this._super();
+    this.onChange(this.get('value'));
+  }
+});
--- app/components/workflow-detail-row.js
+++ app/components/workflow-detail-row.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 const WorkflowDetailRow = Ember.Component.extend({
-  selected: Ember.computed('{workflow,selectedWorkflow}', function () {
-    return this.get('workflow') === this.get('selectedWorkflow');
-  }),
   actions: {
     pick() {
       this.get('onPick')(this.get('workflow'));
