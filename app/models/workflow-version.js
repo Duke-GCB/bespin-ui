ac7fd872b3f3f4dc094a51462815a4f7e66d7dc2
Dan Leehr <dan.leehr@duke.edu>
2016-12-22 09:26:47 -0500

    Adds latestVersion computed property to workflow

--- app/models/workflow-version.js
+++ app/models/workflow-version.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   workflow: DS.belongsTo('workflow'),
+  description: DS.attr('string'),
   objectName: DS.attr('string'),
   created: DS.attr('date'),
   url: DS.attr('string'),
--- app/models/workflow.js
+++ app/models/workflow.js
@@ -1,7 +1,10 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   versions: DS.hasMany('workflow-version'),
-  description: DS.attr('string')
+  latestVersion: Ember.computed('versions', function() {
+    return this.get('versions').get('lastObject');
+  })
 });
