599a25ef4410fe460358227eae1da6257bb22154
Dan Leehr <dan.leehr@duke.edu>
2017-01-09 15:12:44 -0500

    Renames project to dds-project

--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('project', 'Unit | Model | project', {
+moduleForModel('dds-project', 'Unit | Model | project', {
   // Specify the other units that are required for this test.
   needs: []
 });
