46df3a48683ebbe56d81f186305aa68ed05c2ea8
Dan Leehr <dan.leehr@duke.edu>
2017-01-10 10:58:09 -0500

    Fix style issues on models/dds-resource

--- app/models/dds-resource.js
+++ app/models/dds-resource.js
@@ -1,10 +1,10 @@
 import DS from 'ember-data';
-
+import Ember from 'ember';
 let DDSKinds = {file:'dds-file', folder: 'dds-folder'};
 
 export default DS.Model.extend({
   kind: DS.attr('string'),
   name: DS.attr('string'),
-  isFile: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.file}),
-  isFolder: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.folder})
+  isFile: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.file; }),
+  isFolder: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.folder; })
 });
