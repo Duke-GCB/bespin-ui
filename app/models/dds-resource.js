7fd8372631884e8108b25eb3c4c2073557f99a35
Dan Leehr <dan.leehr@duke.edu>
2017-01-11 16:18:59 -0500

    Adds project field to dds-resource model

--- app/models/dds-resource.js
+++ app/models/dds-resource.js
@@ -6,5 +6,6 @@ export default DS.Model.extend({
   kind: DS.attr('string'),
   name: DS.attr('string'),
   isFile: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.file; }),
-  isFolder: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.folder; })
+  isFolder: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.folder; }),
+  project: DS.belongsTo('dds-project')
 });
