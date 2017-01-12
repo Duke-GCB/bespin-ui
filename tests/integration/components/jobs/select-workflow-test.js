79bbe142ccf64e7c68ab9fa2673a8ca627eb1188
Dan Leehr <dan.leehr@duke.edu>
2017-01-12 11:52:56 -0500

    Fixes test broken by reordering select-workflow params

--- tests/integration/components/jobs/select-workflow-test.js
+++ tests/integration/components/jobs/select-workflow-test.js
@@ -7,7 +7,8 @@ moduleForComponent('jobs/select-workflow', 'Integration | Component | jobs/selec
 
 test('it displays the job name', function(assert) {
   this.set('job', {name: 'myjob'});
-  this.render(hbs`{{jobs/select-workflow job}}`);
+  this.set('workflows', []);
+  this.render(hbs`{{jobs/select-workflow workflows job}}`);
   assert.equal(this.$('.job-name input').val(), 'myjob');
 });
 
