2dc3309b73ea5db4472293a656e1eeeda9f2d2cb
Dan Leehr <dan.leehr@duke.edu>
2017-01-24 09:52:34 -0500

    Tests for dds-expand-button

--- tests/integration/components/dds/dds-expand-button-test.js
+++ tests/integration/components/dds/dds-expand-button-test.js
@@ -19,3 +19,26 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), 'template block text');
 });
+
+test('it is not expanded by default', function(assert) {
+  this.render(hbs`{{dds/dds-expand-button}}`);
+  assert.equal(this.$('span.glyphicon-folder-open').length, 0);
+});
+
+test('it binds class name to expanded state', function(assert) {
+  assert.expect(4);
+  this.render(hbs`{{dds/dds-expand-button true}}`);
+  assert.equal(this.$('span.glyphicon-folder-open').length, 1);
+  assert.equal(this.$('span.glyphicon-folder-close').length, 0);
+  this.render(hbs`{{dds/dds-expand-button false}}`);
+  assert.equal(this.$('span.glyphicon-folder-open').length, 0);
+  assert.equal(this.$('span.glyphicon-folder-close').length, 1);
+});
+
+test('it sends action on click', function(assert) {
+  this.on('clicked', function() {
+    assert.ok(true);
+  });
+  this.render(hbs`{{dds/dds-expand-button action="clicked"}}`);
+  this.$('span.dds-expand-button').click();
+});
