537d46318496b5c95b885f45789ab66c9691ba85
Dan Leehr <dan.leehr@duke.edu>
2017-01-25 14:00:15 -0500

    Adds computed property for job-answer isString and isDDSFile

--- app/models/job-answer.js
+++ app/models/job-answer.js
@@ -1,8 +1,21 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+const AnswerKinds = {
+  String: 'string',
+  DDSFile: 'dds_file'
+};
 
 export default DS.Model.extend({
   index: DS.attr('number'),
   kind: DS.attr('string'),
   question: DS.belongsTo('job-question'),
-  questionnaire: DS.belongsTo('job-questionnaire')
+  questionnaire: DS.belongsTo('job-questionnaire'),
+  isString: Ember.computed('kind', function() {
+    return this.get('kind') === AnswerKinds.String;
+  }),
+  isDDSFile: Ember.computed('kind', function() {
+    return this.get('kind') === AnswerKinds.DDSFile;
+  })
+
 });
--- tests/unit/models/job-answer-test.js
+++ tests/unit/models/job-answer-test.js
@@ -1,5 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 import testRelationships from '../../helpers/test-relationships';
+import Ember from 'ember';
 
 moduleForModel('job-answer', 'Unit | Model | job answer', {
   // Specify the other units that are required for this test.
@@ -10,6 +11,22 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('it computes kind', function(assert) {
+  assert.expect(6);
+  let model = this.subject();
+  Ember.run(function() {
+    model.set('kind', 'string');
+    assert.ok(model.get('isString'));
+    assert.notOk(model.get('isDDSFile'));
+    model.set('kind', 'dds_file');
+    assert.notOk(model.get('isString'));
+    assert.ok(model.get('isDDSFile'));
+    model.set('kind', 'alternative');
+    assert.notOk(model.get('isString'));
+    assert.notOk(model.get('isDDSFile'));
+  });
 });
 
 const testRels = [
