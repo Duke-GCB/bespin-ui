a9224ddacc6bfb3f390cc5353de29dc52d649f6d
Dan Leehr <dan.leehr@duke.edu>
2017-02-01 16:21:59 -0500

    Fetches job errors on job show

--- app/components/job-detail.js
+++ app/components/job-detail.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 const JobDetail = Ember.Component.extend({
   job: null,
+  jobErrors: []
 });
 
 JobDetail.reopenClass({
-  positionalParams: ['job']
+  positionalParams: ['job', 'jobErrors']
 });
 
 export default JobDetail;
--- app/routes/jobs/show.js
+++ app/routes/jobs/show.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('job', params.job_id);
+    return Ember.RSVP.hash({
+      job: this.get('store').findRecord('job', params.job_id),
+      errors: this.get('store').query('job-error', {job: params.job_id})
+    });
   }
 });
