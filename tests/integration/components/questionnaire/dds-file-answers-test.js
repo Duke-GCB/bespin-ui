f868ab6e12a3834fa9d4cb12ca5ee613a74c04a5
Dan Leehr <dan.leehr@duke.edu>
2017-02-06 15:38:00 -0500

    Extract mock store to a service

--- tests/integration/components/questionnaire/dds-file-answers-test.js
+++ tests/integration/components/questionnaire/dds-file-answers-test.js
@@ -2,32 +2,35 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 
+const storeStub = Ember.Service.extend({
+  callCount: 0,
+  modelsCalled: [],
+  findAll(modelName) {
+    // For projects!
+    this.get('modelsCalled').push(modelName);
+    this.set('callCount', this.get('callCount') + 1);
+    return new Ember.RSVP.Promise(function (resolve) {
+      resolve([{id:'abc-123', kind: modelName}]);
+    });
+  }
+});
+
 moduleForComponent('questionnaire/dds-file-answer', 'Integration | Component | questionnaire/dds file answers', {
   integration: true,
   beforeEach: function() {
-    this.set('store', Ember.Object.create({
-      callCount: 0,
-      modelsCalled: [],
-      findAll(modelName) {
-        // For projects!
-        this.get('modelsCalled').push(modelName);
-        this.set('callCount', this.get('callCount') + 1);
-        return new Ember.RSVP.Promise(function (resolve) {
-          resolve([{id:'abc-123', kind: modelName}]);
-        });
-      },
-    }));
+    this.register('service:store', storeStub);
+    this.inject.service('store', {as: 'store'});
   }
 });
 
 test('it renders', function(assert) {
-  this.render(hbs`{{questionnaire/dds-file-answers label='Label' store=store}}`);
+  this.render(hbs`{{questionnaire/dds-file-answers label='Label'}}`);
   assert.equal(this.$('label').text(), 'Label');
 });
 
 test('it fetches projects and credentials', function(assert) {
   let store = this.get('store');
-  this.render(hbs`{{questionnaire/dds-file-answers store=store}}`);
+  this.render(hbs`{{questionnaire/dds-file-answers}}`);
   assert.equal(store.get('callCount'), 2);
   // assert.equal with the array is triggering some unrecognizable error
   assert.equal(store.get('modelsCalled')[0], 'dds-project');
