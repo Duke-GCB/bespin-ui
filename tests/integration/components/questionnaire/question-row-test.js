cca090ec64a337d381bf924449f3f5bd7603c960
Dan Leehr <dan.leehr@duke.edu>
2017-02-06 16:37:01 -0500

    Test for question-row name display

--- tests/integration/components/questionnaire/question-row-test.js
+++ tests/integration/components/questionnaire/question-row-test.js
@@ -3,34 +3,14 @@ import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 
 moduleForComponent('questionnaire/question-row', 'Integration | Component | questionnaire/question row', {
-  integration: true,
-  beforeEach: function() {
-    this.set('store', Ember.Object.create({
-      callCount: 0,
-      createRecord(kind, props) {
-        this.set('callCount', this.get('callCount') + 1);
-        return new Ember.RSVP.Promise(function (resolve) {
-          resolve(Ember.Object.create(props));
-        });
-      },
-    }));
-  }
+  integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{questionnaire/question-row}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#questionnaire/question-row}}
-      template block text
-    {{/questionnaire/question-row}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  let questionProxy = Ember.Object.create({
+    question: { name: 'Question 1' }
+  });
+  this.set('questionProxy', questionProxy);
+  this.render(hbs`{{questionnaire/question-row questionProxy}}`);
+  assert.equal(this.$('.question-name').text().trim(), 'Question 1');
 });
