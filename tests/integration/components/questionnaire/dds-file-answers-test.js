b66bfdbcac19cffb2841043f70c40a593690645a
Dan Leehr <dan.leehr@duke.edu>
2017-02-06 15:31:44 -0500

    Tests for dds-file-answers

--- tests/integration/components/questionnaire/dds-file-answers-test.js
+++ tests/integration/components/questionnaire/dds-file-answers-test.js
@@ -7,11 +7,13 @@ moduleForComponent('questionnaire/dds-file-answer', 'Integration | Component | q
   beforeEach: function() {
     this.set('store', Ember.Object.create({
       callCount: 0,
-      findAll() {
+      modelsCalled: [],
+      findAll(modelName) {
         // For projects!
+        this.get('modelsCalled').push(modelName);
         this.set('callCount', this.get('callCount') + 1);
         return new Ember.RSVP.Promise(function (resolve) {
-          resolve([{id:'abc-123'}]);
+          resolve([{id:'abc-123', kind: modelName}]);
         });
       },
     }));
@@ -19,32 +21,15 @@ moduleForComponent('questionnaire/dds-file-answer', 'Integration | Component | q
 });
 
 test('it renders', function(assert) {
+  this.render(hbs`{{questionnaire/dds-file-answers label='Label' store=store}}`);
+  assert.equal(this.$('label').text(), 'Label');
+});
+
+test('it fetches projects and credentials', function(assert) {
+  let store = this.get('store');
   this.render(hbs`{{questionnaire/dds-file-answers store=store}}`);
-  assert.ok(true);
-});
-
-test('it calls onCancel when cancel button clicked', function(assert) {
-  this.set('onAnswer', function() { assert.notOk(true); });
-  this.set('onCancel', function() { assert.ok(true); });
-  this.render(hbs`{{questionnaire/dds-file-answers 0 onAnswer onCancel store=store}}`);
-  this.$('.back-button').click(); // Now calls onCancel
-});
-
-test('it calls onAnswer when next button clicked', function(assert) {
-  this.set('onAnswer', function() { assert.ok(true); });
-  this.set('onCancel', function() { assert.notOk(true); });
-  this.render(hbs`{{questionnaire/dds-file-answers 0 onAnswer onCancel store=store answers=[]}}`);
-  this.$('.next-button').click(); // Now calls onCancel
-});
-
-test('it enables next button when isValid', function(assert) {
-  this.set('occurs', 3); // Require 3 things
-  this.set('pickedFiles', []); // Pick none
-  this.render(hbs`{{questionnaire/dds-file-answers occurs onAnswer onCancel pickedFiles=pickedFiles store=store}}`);
-  assert.equal(this.$('.next-button[disabled]').length, 1, 'next button should be disabled');
-
-  this.set('occurs', 3); // Require 3 things
-  this.set('pickedFiles', [1,2,3]); // Pick 3
-  this.render(hbs`{{questionnaire/dds-file-answers occurs onAnswer onCancel pickedFiles=pickedFiles store=store}}`);
-  assert.equal(this.$('.next-button[disabled]').length, 0, 'next button should not be disabled');
+  assert.equal(store.get('callCount'), 2);
+  // assert.equal with the array is triggering some unrecognizable error
+  assert.equal(store.get('modelsCalled')[0], 'dds-project');
+  assert.equal(store.get('modelsCalled')[1], 'dds-user-credential');
 });
