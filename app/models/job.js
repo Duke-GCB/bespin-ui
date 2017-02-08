1cfe1ffa4bd41becc8975aba6f9e7c3cc6e08dba
Dan Leehr <dan.leehr@duke.edu>
2017-02-08 15:44:18 -0500

    Example of updating the job state after calling a start/cancel/restart action
    - Expects server to respond with {'jobs':{'status':'R'}} even though that's not a job
    - Revisit on resolution of https://github.com/Duke-GCB/bespin-api/issues/22

--- app/models/job.js
+++ app/models/job.js
@@ -12,16 +12,23 @@ export default DS.Model.extend({
   vmProjectName: DS.attr('string'),
   jobOrder: DS.attr('string'), // These will be JSON - can we add a transform?
   outputDir: DS.attr('string'), // Don't have an API for this yet, how do we create output dirs?
+
+  updateAfterAction(data) {
+    Ember.Logger.log(Ember.inspect(data.jobs));
+    this.set('state', data.jobs.status); // Temp hack!
+    return Ember.RSVP.resolve(this.store.peekRecord(this.constructor.modelName, this.get('id')));
+  },
+
   start() {
     let adapter = this.store.adapterFor(this.constructor.modelName);
-    return adapter.start(this.get('id'));
+    return adapter.start(this.get('id')).then(this.updateAfterAction.bind(this));
   },
   cancel() {
     let adapter = this.store.adapterFor(this.constructor.modelName);
-    return adapter.cancel(this.get('id'));
+    return adapter.cancel(this.get('id')).then(this.updateAfterAction.bind(this));
   },
   restart() {
     let adapter = this.store.adapterFor(this.constructor.modelName);
-    return adapter.restart(this.get('id'));
+    return adapter.restart(this.get('id')).then(this.updateAfterAction.bind(this));
   }
 });
