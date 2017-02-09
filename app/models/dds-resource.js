5a0fb65654e83ee707f3bf929501d510927069e3
Dan Leehr <dan.leehr@duke.edu>
2017-02-09 14:36:13 -0500

    Adds size and version to file display
    - Brings in ember-number-to-human-size to format bytes in human-readable format
    - abuses CSS before/after/content rules to put parenthesis and commas
    
    Fixes #6

--- app/models/dds-resource.js
+++ app/models/dds-resource.js
@@ -7,5 +7,8 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   isFile: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.file; }),
   isFolder: Ember.computed('kind', function() { return this.get('kind') === DDSKinds.folder; }),
-  project: DS.belongsTo('dds-project')
+  project: DS.belongsTo('dds-project'),
+  size: DS.attr('number'),
+  version: DS.attr('number'),
+  version_id: DS.attr('string'),
 });
