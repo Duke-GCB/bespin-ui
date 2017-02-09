5f2c74a454076b0f6107cae60f5fa1c6c1eb2f0d
Dan Leehr <dan.leehr@duke.edu>
2017-02-09 11:30:56 -0500

    Updates job-test to verify calls to pushPayload and peekModel on actions

--- tests/unit/models/job-test.js
+++ tests/unit/models/job-test.js
@@ -1,5 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 import { testRelationship } from '../../helpers/test-relationships';
+import Ember from 'ember';
 
 moduleForModel('job', 'Unit | Model | job', {
   // Specify the other units that are required for this test.
@@ -15,28 +16,69 @@ test('it exists', function(assert) {
 testRelationship('job', {key: 'workflowVersion', kind: 'belongsTo', type: 'workflow-version'});
 
 test('it sends actions to the adapter', function(assert) {
-  assert.expect(6); // Two asserts for each action
+  assert.expect(15); // 5asserts for each action
   this.store().set('adapterFor', (modelName) => {
     return {
       start(id) {
-        assert.equal(modelName, 'job');
+        assert.equal(modelName, 'job', 'modelName in adapterFor should be job');
         assert.equal(id, 'startId', 'should call adapter.start() with id');
+        return Ember.RSVP.resolve({id: id, state: 'S'}); // Starting
       },
       cancel(id) {
-        assert.equal(modelName, 'job');
+        assert.equal(modelName, 'job', 'modelName in adapterFor should be job');
         assert.equal(id, 'cancelId', 'should call adapter.cancel() with id');
+        return Ember.RSVP.resolve({id: id, state: 'c'}); // canceling
       },
       restart(id) {
-        assert.equal(modelName, 'job');
+        assert.equal(modelName, 'job', 'modelName in adapterFor should be job');
         assert.equal(id, 'restartId', 'should call adapter.restart() with id');
+        return Ember.RSVP.resolve({id: id, state: 'r'}); // restarting
       }
     };
   });
-  let model = this.subject();
-  model.set('id', 'startId');
-  model.start();
-  model.set('id', 'cancelId');
-  model.cancel();
-  model.set('id', 'restartId');
-  model.restart();
+  this.store().set('peekRecord', (modelName) => {
+    assert.equal(modelName, 'job');
+  });
+  let stubPushPayloadStart = (modelName, data) => {
+    // Returns nothing
+    assert.equal(modelName, 'job');
+    assert.equal(data.state, 'S', 'should push a payload with starting state');
+  };
+
+  let stubPushPayloadCancel = (modelName, data) => {
+    // Returns nothing
+    assert.equal(modelName, 'job');
+    assert.equal(data.state, 'c', 'should push a payload with canceling state');
+  };
+
+  let stubPushPayloadRestart = (modelName, data) => {
+    // Returns nothing
+    assert.equal(modelName, 'job');
+    assert.equal(data.state, 'r', 'should push a payload with restarting state');
+  };
+
+  // Since each one is asynchronous but must be synchronized with the pushPayload function,
+  // these tests are run in 3 sequential Ember.run loops
+
+  let store = this.store();
+  Ember.run(() => {
+    store.set('pushPayload', stubPushPayloadStart);
+    let model = this.subject();
+    model.set('id', 'startId');
+    model.start();
+  });
+
+  Ember.run(() => {
+    store.set('pushPayload', stubPushPayloadCancel);
+    let model = this.subject();
+    model.set('id', 'cancelId');
+    model.cancel();
+  });
+
+  Ember.run(() => {
+    store.set('pushPayload', stubPushPayloadRestart);
+    let model = this.subject();
+    model.set('id', 'restartId');
+    model.restart();
+  });
 });
