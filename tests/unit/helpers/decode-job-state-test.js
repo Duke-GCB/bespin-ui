907606419eba48d8002612350455783c93739288
Dan Leehr <dan.leehr@duke.edu>
2017-02-09 10:42:14 -0500

    Decodes new job states from https://github.com/Duke-GCB/bespin-api/pull/23
    - Streamlines unit tests for step/state decoder helpers

--- tests/unit/helpers/decode-job-state-test.js
+++ tests/unit/helpers/decode-job-state-test.js
@@ -4,25 +4,15 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | decode job state');
 
-// Replace this with your real tests.
 test('it decodes job states', function(assert) {
-  assert.expect(6);
-
-  let r = decodeJobState('R');
-  assert.equal(r, "Running");
-
-  let n = decodeJobState('N');
-  assert.equal(n, "New");
-
-  let f = decodeJobState('F');
-  assert.equal(f, 'Finished');
-
-  let e = decodeJobState('E');
-  assert.equal(e, 'Error');
-
-  let c = decodeJobState('C');
-  assert.equal(c, 'Canceled');
-
-  let u = decodeJobState('_unknown_');
-  assert.equal(u, '_unknown_', 'it returns input for unknown steps');
+  assert.expect(9);
+  assert.equal(decodeJobState('N'), "New");
+  assert.equal(decodeJobState('S'), "Starting");
+  assert.equal(decodeJobState('R'), "Running");
+  assert.equal(decodeJobState('F'), 'Finished');
+  assert.equal(decodeJobState('E'), 'Error');
+  assert.equal(decodeJobState('c'), 'Canceling');
+  assert.equal(decodeJobState('C'), 'Canceled');
+  assert.equal(decodeJobState('r'), 'Restarting');
+  assert.equal(decodeJobState('_unknown_'), '_unknown_', 'it returns input for unknown steps');
 });
