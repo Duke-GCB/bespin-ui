ef654218712d500223fef962214b7600d6d8a4b5
Dan Leehr <dan.leehr@duke.edu>
2017-02-24 16:09:43 -0500

    Add missing docker service and simplify script

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -ti --rm -v $(pwd):/myapp danlynn/ember-cli:2.11.0 ember build --environment production --output-path dist-docker/
-docker build -t quay.io/dukegcb/bespin-ui .
