633be3557b0b39b15c49f55e63a66d622aae15ea
Dan Leehr <dan.leehr@duke.edu>
2017-06-02 10:00:28 -0400

    Extend cookie expiration time in development mode

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -1,7 +1,8 @@
 // app/session-stores/application.js
 import CookieStore from 'ember-simple-auth/session-stores/cookie';
+import ENV from 'bespin-ui/config/environment';
 
 export default CookieStore.extend({
-  cookieExpirationTime: 100,
+  cookieExpirationTime: ENV.environment === 'development' ? 9000 : 100,
   cookieName: 'bespinsession'
 });
