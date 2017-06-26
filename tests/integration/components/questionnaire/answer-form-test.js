4f3a546f8adf1216adad53e24bac5a02a3cab2de
John Bradley <johnbradley2008@gmail.com>
2017-06-26 16:33:55 -0400

    job-name tests pass

--- tests/integration/components/questionnaire/answer-form-test.js
+++ tests/integration/components/questionnaire/answer-form-test.js
@@ -11,5 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{questionnaire/answer-form}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), 'Job Name:');
 });
--- tests/integration/components/questionnaire/job-name-test.js
+++ tests/integration/components/questionnaire/job-name-test.js
@@ -12,14 +12,6 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{questionnaire/job-name}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), 'Job Name:');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#questionnaire/job-name}}
-      template block text
-    {{/questionnaire/job-name}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
