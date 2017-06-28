3b7bc114df91a87d697aaf22df230ea169c072ed
Dan Leehr <dan.leehr@duke.edu>
2017-06-28 14:08:53 -0400

    Change file picker h5 -> label

--- tests/integration/components/questionnaire/file-group-list-test.js
+++ tests/integration/components/questionnaire/file-group-list-test.js
@@ -16,7 +16,7 @@ moduleForComponent('questionnaire/file-group-list', 'Integration | Component | q
 
 test('it renders', function(assert) {
   this.render(hbs`{{questionnaire/file-group-list}}`);
-  assert.equal(this.$('.file-group-list-picker h5').text().trim(), 'Pick your read pairs from Duke Data Service');
-  assert.equal(this.$('.file-group-list-selections h5').text().trim(), 'Selected read pairs');
+  assert.equal(this.$('.file-group-list-picker label').text().trim(), 'Pick your read pairs from Duke Data Service');
+  assert.equal(this.$('.file-group-list-selections label').text().trim(), 'Selected read pairs');
 });
 
