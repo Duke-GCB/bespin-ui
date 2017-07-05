d9758c7ff5a9fdfb0e3cd4092502ee9e7b4f680b
John Bradley <johnbradley2008@gmail.com>
2017-07-05 08:35:20 -0400

    rebuild dukegcb/bespin-web
    Adds deploy trigger that POSTS to dockerhub to rebuild bespin production container.

---
+++ trigger-dockerhub.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Patterned after https://github.com/bioperl/bioperl-live/blob/master/travis_scripts/trigger-dockerhub.sh
+
+set -e
+dockerapi="https://registry.hub.docker.com/u/dukegcb/bespin-web/trigger"
+
+
+if [[ -z "$DOCKERHUB_TOKEN" ]] ; then
+    echo "No API token for Docker Hub, add to repository settings."
+    exit 1
+fi
+
+if [[ "$TRAVIS_BRANCH" != "master" ]] ; then
+    echo "Triggering rebuild of Docker image dukegcb/bespin-web:latest"
+        curl -H "Content-Type: application/json" \
+             --data '{"docker_tag": "latest"}' \
+             -X POST $dockerapi/$DOCKERHUB_TOKEN/
+else
+    echo "Not triggering Docker Hub for branches other than master"
+fi
