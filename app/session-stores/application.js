793eb14c014fbc3a86ad816bbb5cc9fb6310a6fc
Dan Leehr <dan.leehr@duke.edu>
2017-08-09 11:27:01 -0400

    Set cookieExpirationTime to 2 hours
    Fixes #23

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -3,6 +3,6 @@ import CookieStore from 'ember-simple-auth/session-stores/cookie';
 import ENV from 'bespin-ui/config/environment';
 
 export default CookieStore.extend({
-  cookieExpirationTime: ENV.environment === 'development' ? 9000 : 100,
+  cookieExpirationTime: 7200,
   cookieName: 'bespinsession'
 });
