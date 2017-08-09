8c2e4087801d7cf8bb76d87c8ea31c4c0850bdba
Dan Leehr <dan.leehr@duke.edu>
2017-08-09 11:39:27 -0400

    Remove unused import

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -1,6 +1,5 @@
 // app/session-stores/application.js
 import CookieStore from 'ember-simple-auth/session-stores/cookie';
-import ENV from 'bespin-ui/config/environment';
 
 export default CookieStore.extend({
   cookieExpirationTime: 7200,
