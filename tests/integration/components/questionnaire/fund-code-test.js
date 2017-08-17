56345bb7f3f08583190038caf87dd7d2de185cdc
John Bradley <johnbradley2008@gmail.com>
2017-08-17 14:08:57 -0400

    add field to enter fund-code

--- tests/integration/components/questionnaire/fund-code-test.js
+++ tests/integration/components/questionnaire/fund-code-test.js
@@ -1,16 +1,16 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('questionnaire/answer-form', 'Integration | Component | questionnaire/answer form', {
+moduleForComponent('questionnaire/fund-code', 'Integration | Component | questionnaire/fund code', {
   integration: true
 });
 
 test('it renders', function(assert) {
+
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{questionnaire/answer-form}}`);
+  this.render(hbs`{{questionnaire/fund-code}}`);
 
-  assert.notEqual(this.$().text().trim().indexOf('Job Name:'), -1);
-  assert.notEqual(this.$().text().trim().indexOf('Fund Code:'), -1);
+  assert.equal(this.$().text().trim(), 'Fund Code:');
 });
