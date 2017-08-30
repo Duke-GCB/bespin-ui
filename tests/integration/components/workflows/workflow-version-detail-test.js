6e620dca032859e28c79e28f6ade5b91c18262af
John Bradley <johnbradley2008@gmail.com>
2017-08-30 10:28:25 -0400

    fix tests and remove unused components

--- tests/integration/components/workflows/workflow-version-detail-test.js
+++ tests/integration/components/workflows/workflow-version-detail-test.js
@@ -9,17 +9,25 @@ test('it renders', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('workflowVersion', {
+    version: '2',
+    description: 'My workflow',
+    created: 'today',
+    url: 'somewhere.edu',
+  });
 
-  this.render(hbs`{{workflows/workflow-version-detail}}`);
-
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{workflows/workflow-version-detail workflowVersion=workflowVersion}}`);
+  assert.equal(this.$('.workflow-version-details-title').text().trim(), 'Version 2');
+  assert.equal(this.$('.workflow-version-details-description').text().trim(), 'My workflow');
+  assert.equal(this.$('.workflow-version-details-created').text().trim(), 'today');
+  assert.equal(this.$('.workflow-version-details-url').text().trim(), 'somewhere.edu');
 
   // Template block usage:
   this.render(hbs`
-    {{#workflows/workflow-version-detail}}
-      template block text
+    {{#workflows/workflow-version-detail workflowVersion=workflowVersion}}
+      <span class='child-text-block'>template block text</span>
     {{/workflows/workflow-version-detail}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.child-text-block').html().trim(), 'template block text');
 });
