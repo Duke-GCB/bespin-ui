3f4276483d95642b28daaa80947516dac857417f
John Bradley <johnbradley2008@gmail.com>
2017-08-30 11:26:50 -0400

    cleanup versions index route
    uses paramsFor inside index route to access "'parent'" versions parameters
    uses versions.hbs to display workflow name

--- app/routes/workflows/versions/index.js
+++ app/routes/workflows/versions/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    let params = this.paramsFor('workflows.versions');
+    return this.get('store').query('workflow-version', {
+      workflow: params.workflow_id
+    });
+  }
 });
