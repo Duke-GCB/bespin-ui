d7492f289b019a6a864191ac5b4be4d23da710a8
John Bradley <johnbradley2008@gmail.com>
2017-08-31 15:55:45 -0400

    move error list into component

---
+++ tests/integration/components/error-panel-test.js
@@ -0,0 +1,32 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('error-panel', 'Integration | Component | error panel', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('noErrors', [])
+  this.set('oneError', [{
+    'attribute': 'jobName',
+    'message': 'required',
+  }]);
+
+  this.render(hbs`{{error-panel errors=noErrors}}`);
+  assert.equal(this.$().text().trim(), '');
+
+  this.render(hbs`{{error-panel errors=oneError}}`);
+  assert.equal(this.$('li').text(), 'jobName - required');
+
+
+  this.render(hbs`
+    {{#error-panel errors=oneError}}
+      <span class='childtext'>template block text</span>
+    {{/error-panel}}
+  `);
+
+  assert.equal(this.$('.childtext').text().trim(), 'template block text');
+});
