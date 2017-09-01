dd17e0802cba44dc58556b142e69f9c250cd0319
John Bradley <johnbradley2008@gmail.com>
2017-09-01 10:05:40 -0400

    test select-workflow-version transitionTo

--- tests/unit/controllers/jobs/new/select-workflow-version-test.js
+++ tests/unit/controllers/jobs/new/select-workflow-version-test.js
@@ -10,3 +10,26 @@ test('it exists', function(assert) {
   let controller = this.subject();
   assert.ok(controller);
 });
+
+test('it handles back action', function(assert) {
+  let controller = this.subject({
+    transitionToRoute(routeName) {
+      assert.equal(routeName, 'jobs.new.select-workflow','back action should transition to select-workflow');
+    }
+  });
+  controller.send('back');
+});
+
+test('it handles next action', function(assert) {
+  const workflowVersion = {
+    id: '2',
+  };
+  let controller = this.subject({
+    workflowVersion: workflowVersion,
+    transitionToRoute(routeName, id) {
+      assert.equal(routeName, 'jobs.new.select-questionnaire', 'next action should transition to questionnaire selection');
+      assert.equal(id, '2', 'next action should extract workflow version id');
+    }
+  });
+  controller.send('next');
+});
