74d1ade9771d6c7dae65b0417a7b296f179283c6
John Bradley <johnbradley2008@gmail.com>
2017-09-01 09:44:02 -0400

    test workflow-version-card pick action

--- tests/integration/components/jobs/workflow-version-card-test.js
+++ tests/integration/components/jobs/workflow-version-card-test.js
@@ -6,18 +6,23 @@ moduleForComponent('jobs/workflow-version-card', 'Integration | Component | jobs
 });
 
 test('it renders', function(assert) {
+  assert.expect(5);
 
-  this.set('workflowVersion', {
-      workflow: {name: 'Exome Seq'},
-      version: '1'
-    }
-  );
-
-  this.render(hbs`{{jobs/workflow-version-card workflowVersion=workflowVersion}}`);
+  const workflowVersion = {
+    workflow: {name: 'Exome Seq'},
+    version: '1'
+  };
+  this.set('workflowVersion', workflowVersion);
+  this.set('onPicked', function(item) {
+    assert.equal(item, workflowVersion);
+  });
+  this.render(hbs`{{jobs/workflow-version-card workflowVersion=workflowVersion onPicked=(action onPicked)}}`);
 
   assert.equal(this.$('.jobs-workflow-version-card-title').text().trim(), 'Exome Seq');
   assert.equal(this.$('.jobs-workflow-version-card-version').text().trim(), 'v1');
-  assert.equal(this.$('input').attr('type'), 'radio');
+  assert.equal(this.$('input[name=selectedItem]').attr('type'), 'radio');
+  this.$('input[name=selectedItem]').click();
+
 
   // Template block usage:
   this.render(hbs`
