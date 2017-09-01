126b49950a3145272ed1a575b73b3951e72184d2
John Bradley <johnbradley2008@gmail.com>
2017-09-01 11:44:37 -0400

    test workflows/versions/index model method

--- tests/unit/routes/workflows/versions/index-test.js
+++ tests/unit/routes/workflows/versions/index-test.js
@@ -2,10 +2,24 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:workflows/versions/index', 'Unit | Route | workflows/versions/index', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['model:workflow-version']
 });
 
 test('it exists', function(assert) {
+  assert.expect(4)
+  function mockParamsFor(name) {
+    assert.equal(name, 'workflows.versions');
+    return {
+      workflow_id: 123
+    };
+  }
+  function mockQuery(name, param) {
+    assert.equal(name, 'workflow-version');
+    assert.equal(param.workflow, 123);
+  }
   let route = this.subject();
+  route.paramsFor = mockParamsFor;
+  route.get('store').query = mockQuery;
   assert.ok(route);
+  route.model();
 });
