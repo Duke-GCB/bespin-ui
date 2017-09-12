1562db99b6f41f51a3bb0f8f9e4409d2b5a6b00a
John Bradley <johnbradley2008@gmail.com>
2017-09-12 16:03:48 -0400

    revert all dds-resouce-header changes

--- app/components/dds/dds-resource-header.js
+++ app/components/dds/dds-resource-header.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  title: 'Select All Files',
   icon: 'glyphicon-plus',
   tagName: 'span',
   classNames: ['dds-resource-header'],
-  title: 'Select All Files',
   click() {
     this.sendAction();
   }
