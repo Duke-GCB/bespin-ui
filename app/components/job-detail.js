9ad09ce2daafd0bbaa19cdae540129084b8e3d60
Dan Leehr <dan.leehr@duke.edu>
2017-09-21 13:53:59 -0400

    Only show authorize form when job needs authorization or is just-authorized

--- app/components/job-detail.js
+++ app/components/job-detail.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 const JobDetail = Ember.Component.extend({
   tagName: 'div',
   classNames: ['job-detail'],
-  job: null
+  job: null,
+  showAuthorize: Ember.computed('job.hasAuthorization', 'job.isAuthorized', function() {
+    // Only show the authorize control if the job needsAuthorization or is in the just-authorized state
+    return !this.get('job.hasAuthorization') || this.get('job.isAuthorized');
+  })
 });
 
 JobDetail.reopenClass({
