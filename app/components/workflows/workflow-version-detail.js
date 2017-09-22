48ab00f125529ad2c252d91aacdfa1a4b8711517
Dan Leehr <dan.leehr@duke.edu>
2017-09-22 14:30:54 -0400

    Compute workflow-version-details title in component
    Allows it to be overridden

--- app/components/workflows/workflow-version-detail.js
+++ app/components/workflows/workflow-version-detail.js
@@ -2,4 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   workflowVersion: null,
+  title: Ember.computed('workflowVersion.version', function() {
+    return `Version ${this.get('workflowVersion.version')}`;
+  })
 });
