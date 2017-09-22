baa8a66dc7708a2fabd7e14c4b1fc86ae6e570a5
Dan Leehr <dan.leehr@duke.edu>
2017-09-22 14:35:44 -0400

    Puts workflow name into workflow card in job-summary

--- app/components/job-summary.js
+++ app/components/job-summary.js
@@ -4,7 +4,11 @@ const JobSummary = Ember.Component.extend({
   tagName: 'div',
   classNames: ['row', 'job-summary'],
   job: null,
-
+  workflowTitle: Ember.computed('job.workflowVersion.workflow.name', 'job.workflowVersion.version', function() {
+    const workflowName = this.get('job.workflowVersion.workflow.name');
+    const versionNumber = this.get('job.workflowVersion.version');
+    return `${workflowName} - Version ${versionNumber}`;
+  })
 });
 
 JobSummary.reopenClass({
