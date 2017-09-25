ae3e671cac9fb00209d9bbe9020307a6a577ad0a
Dan Leehr <dan.leehr@duke.edu>
2017-09-25 11:35:03 -0400

    Tests for job-oncompletion-detail

--- tests/integration/components/job-oncompletion-detail-test.js
+++ tests/integration/components/job-oncompletion-detail-test.js
@@ -1,25 +1,26 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('job-oncompletion-detail', 'Integration | Component | job oncompletion detail', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test('it renders two paragraphs', function(assert) {
   this.render(hbs`{{job-oncompletion-detail}}`);
+  assert.equal(this.$('p.body').length, 2);
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('it renders share group', function(assert) {
+  this.set('email', 'name@example.com');
+  this.set('shareGroup', Ember.Object.create({name: 'TEST GROUP'}));
+  this.render(hbs`{{job-oncompletion-detail shareGroup email}}`);
+  assert.ok(this.$().text().indexOf('If the TEST GROUP has any feedback on the data'));
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#job-oncompletion-detail}}
-      template block text
-    {{/job-oncompletion-detail}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it renders email address if present', function(assert) {
+  this.set('email', 'name@example.com');
+  this.set('shareGroup', Ember.Object.create());
+  this.render(hbs`{{job-oncompletion-detail shareGroup email}}`);
+  assert.ok(this.$().text().indexOf('You will receive an email at name@example.com when the job finishes'));
 });
