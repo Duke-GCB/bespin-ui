fb9d5804f5e6197a4ef3b1df419dbc95a8b02c1d
Dan Leehr <dan.leehr@duke.edu>
2017-09-25 11:49:37 -0400

    Tests for job-state component

--- tests/integration/components/job-state-test.js
+++ tests/integration/components/job-state-test.js
@@ -1,25 +1,48 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('job-state', 'Integration | Component | job state', {
   integration: true
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{job-state}}`);
+  assert.equal(this.$('.job-state').length, 1);
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('it renders job properties', function(assert) {
+  let job = Ember.Object.create({
+    id: 314,
+    state: 'R'
+  });
+  this.set('job',job);
+  this.render(hbs`{{job-state job}}`);
+  assert.equal(this.$('dd.job-id').text().trim(), '314');
+  assert.equal(this.$('dd.state').text().trim(), 'Running');
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#job-state}}
-      template block text
-    {{/job-state}}
-  `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it shows authorization code if job has a runToken', function(assert) {
+  let job = Ember.Object.create();
+  this.set('job', job);
+  Ember.run(() => {
+    this.render(hbs`{{job-state job}}`);
+    assert.equal(this.$('dd.run_token').length, 0);
+    this.set('job.runToken', 'abc123');
+    assert.equal(this.$('dd.run_token').length, 1);
+    assert.equal(this.$('dd.run_token').text().trim(), 'abc123');
+  });
+});
+
+test('it shows decoded job step if step is not empty', function(assert) {
+  let job = Ember.Object.create();
+  this.set('job', job);
+  Ember.run(() => {
+    this.render(hbs`{{job-state job}}`);
+    assert.equal(this.$('dd.step').length, 0);
+    this.set('job.step', 'S');
+    assert.equal(this.$('dd.step').length, 1);
+    assert.equal(this.$('dd.step').text().trim(), 'Staging In');
+  });
 });
--- tests/integration/components/job-status-results-test.js
+++ tests/integration/components/job-status-results-test.js
@@ -5,21 +5,9 @@ moduleForComponent('job-status-results', 'Integration | Component | job status r
   integration: true
 });
 
+// This test is pretty barebones, pending https://github.com/Duke-GCB/bespin-ui/issues/70
+
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{job-status-results}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#job-status-results}}
-      template block text
-    {{/job-status-results}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '/');
 });
