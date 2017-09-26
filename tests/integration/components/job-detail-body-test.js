23265f0f826050d5c34d5187cbc51ab79fc89bca
Dan Leehr <dan.leehr@duke.edu>
2017-09-26 10:45:30 -0400

    Fixes job-detail-body to show job-authorize when isNew
    Fills out tests for this component too

--- tests/integration/components/job-detail-body-test.js
+++ tests/integration/components/job-detail-body-test.js
@@ -13,3 +13,66 @@ test('it renders', function(assert) {
   this.render(hbs`{{job-detail-body job}}`);
   assert.notEqual(this.$('p').text().trim(), '');
 });
+
+const states = [
+  'isNew',
+  'isAuthorized',
+  'isStarting',
+  'isRunning',
+  'isFinished',
+  'isErrored',
+  'isCanceling',
+  'isCanceled',
+  'isRestarting'];
+
+test('it renders job-authorize only for isNew and isAuthorized,', function(assert) {
+  assert.expect(states.length); // 2 assertions per state
+  states.forEach((state) => {
+    let job = Ember.Object.create();
+    job.set(state, true);
+    this.set('job', job);
+    this.render(hbs`{{job-detail-body job}}`);
+    if(state === 'isNew' || state === 'isAuthorized') {
+      assert.equal(this.$('.job-authorize').length, 1, `.job-authorize should be rendered when job.${state} is true`);
+    } else {
+      assert.equal(this.$('.job-authorize').length, 0, `.job-authorize should not be rendered when job.${state} is false`);
+    }
+  });
+});
+
+test('it renders job-error only for isError,', function(assert) {
+  assert.expect(states.length); // 2 assertions per state
+  states.forEach((state) => {
+    let job = Ember.Object.create();
+    job.set(state, true);
+    this.set('job', job);
+    this.render(hbs`{{job-detail-body job}}`);
+    if(state === 'isErrored') {
+      assert.equal(this.$('.job-error').length, 1, `.job-error should be rendered when job.${state} is true`);
+    } else {
+      assert.equal(this.$('.job-error').length, 0, `.job-error should not be rendered when job.${state} is false`);
+    }
+  });
+});
+
+test('it renders text for all valid states', function (assert) {
+  assert.expect(states.length);
+  states.forEach((state) => {
+    let job = Ember.Object.create();
+    job.set(state, true);
+    this.set('job', job);
+    this.render(hbs`{{job-detail-body job}}`);
+    assert.notEqual(this.$().text().trim(), '');
+  });
+});
+
+test('it renders no text for invalid valid states', function (assert) {
+  assert.expect(states.length + 1);
+  let job = Ember.Object.create({});
+  states.forEach((state) => {
+    assert.notOk(job.get(state), `${state} is a valid state, job should not be in a valid state`);
+  });
+  this.set('job', job);
+  this.render(hbs`{{job-detail-body job}}`);
+  assert.equal(this.$().text().trim(), '');
+});
