bb0fd15896b97a998e8c1f7ca012358f171fb2a8
John Bradley <johnbradley2008@gmail.com>
2017-09-26 16:43:05 -0400

    workflow details as markdown
    adds moment module to format date

--- app/components/workflows/workflow-version-detail.js
+++ app/components/workflows/workflow-version-detail.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   workflowVersion: null,
+  workflowMarkdown: Ember.computed('workflowVersion.methodsDocument.contents', function() {
+    const descriptionHeader = '### Description';
+    const description = this.get('workflowVersion.description');
+    const methodsMarkdown = this.get('workflowVersion.methodsDocument.contents');
+    return `${descriptionHeader}\n${description}\n\n${methodsMarkdown}`;
+  })
 });
