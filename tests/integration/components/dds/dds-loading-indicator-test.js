793ed266188a0f2b1250b1bfb0d786459b59efde
Dan Leehr <dan.leehr@duke.edu>
2017-10-02 14:49:28 -0400

    Adds a dds-loading-indicator component
    Includes CSS animations to spin

---
+++ tests/integration/components/dds/dds-loading-indicator-test.js
@@ -0,0 +1,11 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('dds/dds-loading-indicator', 'Integration | Component | dds/dds loading indicator', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`{{dds/dds-loading-indicator}}`);
+  assert.equal(this.$().text().trim(), 'Loading...');
+});
