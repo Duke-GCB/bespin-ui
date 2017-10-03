b59a4643f418fa280579c98caf38bef670cbf5db
Dan Leehr <dan.leehr@duke.edu>
2017-10-03 10:57:39 -0400

    dds-project-files-picker shows empty/loading components

--- tests/integration/components/dds/dds-project-files-picker-test.js
+++ tests/integration/components/dds/dds-project-files-picker-test.js
@@ -5,7 +5,21 @@ moduleForComponent('dds/dds-project-files-picker', 'Integration | Component | dd
   integration: true
 });
 
-test('it renders two form groups', function(assert) {
-  this.render(hbs`{{dds/dds-project-files-picker}}`);
+test('it renders no form groups when loading', function(assert) {
+  this.set('isLoading', true);
+  this.render(hbs`{{dds/dds-project-files-picker isLoading=isLoading}}`);
+  assert.equal(this.$('.form-group').length, 0);
+});
+
+test('it renders no form groups when empty', function(assert) {
+  this.set('isEmpty', true);
+  this.render(hbs`{{dds/dds-project-files-picker isEmpty=isEmpty}}`);
+  assert.equal(this.$('.form-group').length, 0);
+});
+
+test('it renders two form groups when populated', function(assert) {
+  this.set('isLoading', false);
+  this.set('isEmpty', false);
+  this.render(hbs`{{dds/dds-project-files-picker isLoading=isLoading isEmpty=isEmpty}}`);
   assert.equal(this.$('.form-group').length, 2);
 });
