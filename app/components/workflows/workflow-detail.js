02f6d5129890b038694b9428d72906cc50c16d21
John Bradley <johnbradley2008@gmail.com>
2017-10-03 11:48:05 -0400

    navigate workflow versions
    Displays "(Current)" for current workflow version.

--- app/components/workflows/workflow-detail.js
+++ app/components/workflows/workflow-detail.js
@@ -2,4 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   workflow: null,
+  sortedVersions: Ember.computed('workflow.versions', function() {
+    return this.get('workflow.versions').sortBy('version').reverse();
+  })
 });
