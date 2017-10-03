7f63b27ad2c891a9859822963ac238441a165940
John Bradley <johnbradley2008@gmail.com>
2017-10-03 08:44:30 -0400

    Add missing computed field for workflowMarkdown

--- app/components/workflows/workflow-version-detail.js
+++ app/components/workflows/workflow-version-detail.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   workflowVersion: null,
-  workflowMarkdown: Ember.computed('workflowVersion.methodsDocument.contents', function() {
+  workflowMarkdown: Ember.computed('workflowVersion.description', 'workflowVersion.methodsDocument.contents', function() {
     const descriptionHeader = '### Description';
     const description = this.get('workflowVersion.description');
     const methodsMarkdown = this.get('workflowVersion.methodsDocument.contents');
