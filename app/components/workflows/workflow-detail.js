3bf5cd3e84f6798d5e3af6f736595a526eced20c
John Bradley <johnbradley2008@gmail.com>
2017-10-03 16:33:26 -0400

    Only show current workflow version in workflows list
    If user drills down into the workflow they will see all versions.

--- app/components/workflows/workflow-detail.js
+++ app/components/workflows/workflow-detail.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   workflow: null,
-  sortedVersions: Ember.computed('workflow.versions', function() {
-    return this.get('workflow.versions').sortBy('version').reverse();
+  onlyShowCurrent: false,
+  sortedVersions: Ember.computed('onlyShowCurrent', 'workflow.versions', function() {
+    const versions = this.get('workflow.versions').sortBy('version').reverse();
+    if (this.get('onlyShowCurrent')) {
+      return versions.slice(0, 1);
+    }
+    return versions;
   })
 });
