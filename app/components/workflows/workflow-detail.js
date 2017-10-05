8bac18267246c7fc0a9a9f0cb13131ef5514ee03
John Bradley <johnbradley2008@gmail.com>
2017-10-05 08:26:23 -0400

    improve requirements for sortedVersions computed field

--- app/components/workflows/workflow-detail.js
+++ app/components/workflows/workflow-detail.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   workflow: null,
   onlyShowCurrent: false,
-  sortedVersions: Ember.computed('onlyShowCurrent', 'workflow.versions', function() {
+  sortedVersions: Ember.computed('onlyShowCurrent', 'workflow.versions[]', function() {
     const versions = this.get('workflow.versions').sortBy('version').reverse();
     if (this.get('onlyShowCurrent')) {
       return versions.slice(0, 1);
