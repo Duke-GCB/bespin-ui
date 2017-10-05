7f44cee9189891cc4d20ade2b4ed07fc6268e0c3
Dan Leehr <dan.leehr@duke.edu>
2017-10-05 15:27:59 -0400

    Displays current user in navbar
    Adds user model
    application route fetches /users/current-user

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,3 +1,10 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-export default Ember.Route.extend(ApplicationRouteMixin, {});
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  setupController(controller, model) {
+   this._super(controller, model);
+    this.get('store').findRecord('user', 'current-user').then(function(currentUser) {
+      controller.set('currentUser', currentUser);
+    });
+  }
+});
