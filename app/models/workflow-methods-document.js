7564b297fd47e78d6821a04cdcd7c9c87f5192a5
John Bradley <johnbradley2008@gmail.com>
2017-10-05 08:32:49 -0400

    Comment explaining content and contents

--- app/models/workflow-methods-document.js
+++ app/models/workflow-methods-document.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 
 export default DS.Model.extend({
   workflowVersion: DS.belongsTo('workflow-version'),
+  // content is a reserved name so we create `contents` for use in templates
   content: DS.attr('string'),
   contents: Ember.computed.alias('content'),
 });
