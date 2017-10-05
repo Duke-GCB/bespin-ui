4ab5ae3985a4e341f39526b5b85b27122d8cf6d6
Dan Leehr <dan.leehr@duke.edu>
2017-10-05 15:49:39 -0400

    Test for user service

--- tests/unit/services/user-test.js
+++ tests/unit/services/user-test.js
@@ -1,12 +1,31 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
-moduleFor('service:user', 'Unit | Service | user', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+const UserStoreStub = Ember.Object.extend({
+  findRecord(modelName, id) {
+    return Ember.RSVP.resolve({
+      modelName: modelName,
+      id: id,
+      username: 'abc123'
+    });
+  }
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+moduleFor('service:user', 'Unit | Service | user', {
+  needs: ['model:user'],
+  beforeEach() {
+    this.register('service:store', UserStoreStub);
+    this.inject.service('store', {as: 'store'});
+  }
+});
+
+test('it queries users/current-user from the store', function(assert) {
+  assert.expect(4);
   let service = this.subject();
   assert.ok(service);
+  service.currentUser().then(function(user) {
+    assert.equal(user.modelName, 'user');
+    assert.equal(user.id, 'current-user');
+    assert.equal(user.username, 'abc123');
+  });
 });
