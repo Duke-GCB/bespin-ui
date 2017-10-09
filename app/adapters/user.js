faa81c9326016d8e424e362171e92375b58b7888
Dan Leehr <dan.leehr@duke.edu>
2017-10-09 12:00:13 -0400

    Update user adapter to just override urlForQueryRecord

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,11 +1,18 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-  queryRecord(modelName, query) {
-    Ember.Logger.log('querying record');
-    let url = 'http://localhost:8000/api/users/current-user/';
-    Ember.Logger.log(url);
-    // return Ember.$.getJSON(url);
-    return this.ajax(url, 'GET', {});
+  urlForQueryRecord(query, modelName) {
+    /*
+    From https://emberjs.com/api/ember-data/2.16.0/classes/DS.Store/methods/queryRecord?anchor=queryRecord
+
+    > queryRecord makes a request for one record, where the id is not known beforehand
+    > (if the id is known, use findRecord instead).
+
+    Initially tried findRecord, passing 'current-user' as the ID. But this causes Ember Data to raise warnings
+    about the id 'current-user' not matching the record's id returned in the payload.
+
+    So instead, they recommend using queryRecord, and we can simply override urlForQueryRecord
+    */
+    return this.buildURL(modelName, 'current-user');
   }
 });
--- app/services/user.js
+++ app/services/user.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   store: Ember.inject.service(),
   currentUser() {
+    // Use queryRecord since we don't know the id ahead of time
     return this.get('store').queryRecord('user', {});
   }
 });
