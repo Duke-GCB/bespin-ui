87609aef6514c53200210790ed88f9d4a8ff2e7d
John Bradley <johnbradley2008@gmail.com>
2017-10-10 12:06:42 -0400

    remove unused duplicate route

--- app/routes/jobs/readme/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model(params) {
-    console.log(params);
-    return params;
-    //return this.get('store').findRecord('job', params.job_id);
-  }
-});
