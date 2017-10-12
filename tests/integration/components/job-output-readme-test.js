816f00a68e78c5c01beb893d523391e8737ede74
John Bradley <johnbradley2008@gmail.com>
2017-10-12 10:01:31 -0400

    tests for job-output-readme component

--- tests/integration/components/job-output-readme-test.js
+++ tests/integration/components/job-output-readme-test.js
@@ -1,17 +1,47 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+//Stub location service
+const ddsUrlFetcherStub = Ember.Service.extend({
+  fetchReadmeUrl(outputDir) {
+    return {
+      then(onData, onFail) {
+        if (outputDir.stubHint == 'loaddata') {
+          onData("MarkdownData")
+        }
+        if (outputDir.stubHint == 'showerror') {
+          onFail({
+            errors: [{
+              status: 404,
+              detail: "Not Found"
+            }]
+          })
+        }
+      }
+    };
+  }
+});
 
 moduleForComponent('job-output-readme', 'Integration | Component | job output readme', {
-  integration: true
+  integration: true,
+  beforeEach: function () {
+    this.register('service:dds-url-fetcher', ddsUrlFetcherStub);
+    this.inject.service('dds-url-fetcher', { as: 'ddsUrlFetcher' });
+  }
 });
 
 test('it renders loading', function(assert) {
-  this.render(hbs`{{job-output-readme loading=true}}`);
+  this.set('outputDir', {
+    stubHint: 'neverload'
+  });
+
+  this.render(hbs`{{job-output-readme outputDir=outputDir}}`);
   assert.equal(this.$().text().trim(), 'Loading...');
 
   // Template block usage:
   this.render(hbs`
-    {{#job-output-readme loading=true}}
+    {{#job-output-readme outputDir=outputDir}}
       template block text
     {{/job-output-readme}}
   `);
@@ -19,7 +49,17 @@ test('it renders loading', function(assert) {
 });
 
 test('it renders content for readme', function(assert) {
-  this.set('readmeMarkdown', "Stuff");
-  this.render(hbs`{{job-output-readme readmeMarkdown=readmeMarkdown loading=false}}`);
-  assert.equal(this.$().text().trim(), 'Stuff');
+  this.set('outputDir', {
+    stubHint: 'loaddata'
+  });
+  this.render(hbs`{{job-output-readme outputDir=outputDir}}`);
+  assert.equal(this.$().text().trim(), 'MarkdownData');
+});
+
+test('it renders error on failure for readme', function(assert) {
+  this.set('outputDir', {
+    stubHint: 'showerror'
+  });
+  this.render(hbs`{{job-output-readme outputDir=outputDir}}`);
+  assert.equal(this.$('.error-details').text().trim(), 'Not Found');
 });
