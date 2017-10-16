8f27f19e35b174152a2e802490791da86ec30786
Dan Leehr <dan.leehr@duke.edu>
2017-10-16 10:46:15 -0400

    Adds modal-confirmation to confirm before deleting jobs

--- app/components/job-detail-row.js
+++ app/components/job-detail-row.js
@@ -7,8 +7,13 @@ const JobDetailRow = Ember.Component.extend({
     delete() {
       this.get('job').destroyRecord();
     }
-  }
-
+  },
+  modalConfirmationTitle: Ember.computed('job.id', function() {
+    return `Are you sure you want to delete job ${this.get('job.id')}?`;
+  }),
+  modalConfirmationBody: Ember.computed('job.name', function() {
+    return `Your Bespin job '${this.get('job.name')}' will be deleted permanently. This action cannot be undone.`
+  })
 });
 
 JobDetailRow.reopenClass({
