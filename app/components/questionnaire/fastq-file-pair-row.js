733f0eb3e9943d084dd7133b4ecc216cd43f54f0
Dan Leehr <dan.leehr@duke.edu>
2017-10-20 14:23:08 -0400

    Indicate sample name and errors in fastq-file-pair-row
    Tried making this editable, will revisit later.

--- app/components/questionnaire/fastq-file-pair-row.js
+++ app/components/questionnaire/fastq-file-pair-row.js
@@ -2,7 +2,25 @@ import Ember from 'ember';
 import FileGroupRow from 'bespin-ui/components/questionnaire/file-group-row';
 
 const FASTQFilePairRow = FileGroupRow.extend({
-  pairName: Ember.computed.alias('pair.name'),
+  pairName: Ember.computed.oneWay('pair.name'),
+  errors: Ember.computed('pair.file1', 'pair.file2', 'pair.name', function() {
+    const errors = [];
+    const file1 = this.get('pair.file1');
+    const file2 = this.get('pair.file2');
+    const name = this.get('pair.name');
+    let message = '';
+    // If no files are chosen, indicate this is automatic
+    if(Ember.isEmpty(file1) || Ember.isEmpty(file2)) {
+      message = 'Select two files to detect sample name.';
+    } else if(Ember.isEmpty(name)) {
+      // If files are chosen, indicate the automatic naming failed
+      message = 'No sample name detected. File names must begin with a common name to detect sample name.';
+    }
+    if(message) {
+      errors.addObject(Ember.Object.create({message: message}));
+    }
+    return errors;
+  })
 });
 
 FASTQFilePairRow.reopenClass({
