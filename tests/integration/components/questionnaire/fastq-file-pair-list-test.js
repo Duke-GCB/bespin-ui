7d8024708d275ff57f650e4ab807d670135a2fdf
Dan Leehr <dan.leehr@duke.edu>
2017-10-27 10:37:40 -0400

    Tests that changing field errors is rendered

--- tests/integration/components/questionnaire/fastq-file-pair-list-test.js
+++ tests/integration/components/questionnaire/fastq-file-pair-list-test.js
@@ -34,3 +34,22 @@ test('it renders a fastq-file-pair-row for each selected pair', function(assert)
   this.render(hbs`{{questionnaire/fastq-file-pair-list fileItems=fileItems}}`);
   assert.equal(this.$('.fastq-file-pair-row').length, 2)
 });
+
+test('it correctly observes error array', function(assert) {
+  const errors = Ember.Object.create({
+    show: true,
+    errors: [{field: 'field-name', message: 'Empty'}],
+    setError() { }
+  });
+  this.set('answerFormErrors', errors);
+  this.set('fieldName', 'field-name');
+  Ember.run(() => {
+    // Initially empty
+    this.render(hbs`{{questionnaire/fastq-file-pair-list fieldName=fieldName answerFormErrors=answerFormErrors}}`);
+    assert.equal(this.$('.error-panel').text().trim(), 'Empty');
+
+    // Now replace the errors and verify the new error is displayed
+    this.set('answerFormErrors.errors', [{field: 'field-name', message: 'Incomplete'}]);
+    assert.equal(this.$('.error-panel').text().trim(), 'Incomplete');
+  });
+});
