138329cb9cf4d5bb16dd9241dc164cf35ad14468
Dan Leehr <dan.leehr@duke.edu>
2018-01-13 17:56:36 -0500

    Use ember-drf-frontend functionality
    - Build fails in broccoli due to babel plugin issue.
    - Suspect we need to upgrade ember-cli-babel to 6.6 or later for new module imports

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,38 +1,7 @@
-import DS from 'ember-data';
-import Ember from 'ember';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin'; // This is what causes the authorizer to be picked up
+import DRFAdapter 'drf-ember-frontend/adapters/drf-adapter';
 import ENV from 'bespin-ui/config/environment'; // This is how we load config variables from our environment.js file
 
-export default DS.RESTAdapter.extend(DataAdapterMixin, {
-  /*
-    This adapter is customized for bespin-api running Django REST Framework
-   */
-  authorizer: 'authorizer:drf-token-authorizer', // Adds token authorization to requests
+export default DRFAdapter.extend(DataAdapterMixin, {
   host: ENV.APP.API_URL,
   namespace: ENV.APP.API_NAMESPACE,
-  headers: {
-    'Accept': 'application/vnd.rootobject+json',
-    'Content-type': 'application/vnd.rootobject+json'
-  },
-  buildURL(modelName, id, snapshot, requestType, query) {
-    var url = this._super(modelName, id, snapshot, requestType, query);
-    // Enforce trailing slashes
-    if (url.charAt(url.length - 1) !== '/') {
-      url += '/';
-    }
-    return url;
-  },
-  /**
-   * pathForType returns the path on the API server for the given model type
-   * We override it here because Ember's default is to camelcase and pluralize
-   * @param type
-   */
-  pathForType(type) {
-    let dasherized = Ember.String.dasherize(type);
-    let pluralized = Ember.String.pluralize(dasherized);
-    return pluralized;
-  },
-  isInvalid(status) {
-    return status === 400;
-  },
 });
