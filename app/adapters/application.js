1e4c34532e37be84f4c9a9217f139b2370dd890c
Dan Leehr <dan.leehr@duke.edu>
2018-01-16 10:47:15 -0500

    Fix syntax errors in application adapter
    These caused a Broccoli mergeTrees error that I tried to debug by deleting node_modules, package-lock.json, and re-running npm install
    Seems that reinstall was not necessary, but the reinstall did upgrade broccoli components to show better errors, so I'll keep it

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
-import DRFAdapter 'drf-ember-frontend/adapters/drf-adapter';
+import DRFAdapter from 'drf-ember-frontend/adapters/drf-adapter';
 import ENV from 'bespin-ui/config/environment'; // This is how we load config variables from our environment.js file
 
-export default DRFAdapter.extend(DataAdapterMixin, {
+export default DRFAdapter.extend({
   host: ENV.APP.API_URL,
   namespace: ENV.APP.API_NAMESPACE,
 });
