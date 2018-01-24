3bed3c45a1cf6a19f2b197dd409b609933a5f9ce
Dan Leehr <dan.leehr@duke.edu>
2018-01-24 09:42:40 -0500

    Update fastq-file-pair-row.js to use FASTQSample computed properties

--- app/components/questionnaire/fastq-file-pair-row.js
+++ app/components/questionnaire/fastq-file-pair-row.js
@@ -3,16 +3,15 @@ import FileGroupRow from 'bespin-ui/components/questionnaire/file-group-row';
 
 const FASTQFilePairRow = FileGroupRow.extend({
   classNames: ['fastq-file-pair-row', 'well','well-sm'],
-  pairName: Ember.computed.oneWay('pair.name'),
-  errors: Ember.computed('pair.file1', 'pair.file2', 'pair.name', function() {
+  pairName: Ember.computed.alias('pair.sampleName'),
+  errors: Ember.computed('pair.isFull', 'pair.sampleName', function() {
     const errors = [];
-    const file1 = this.get('pair.file1');
-    const file2 = this.get('pair.file2');
-    const name = this.get('pair.name');
+    const files = this.get('pair.fileItems');
+    const name = this.get('pair.sampleName');
     let message = '';
     // If no files are chosen, indicate this is automatic
-    if(Ember.isEmpty(file1) || Ember.isEmpty(file2)) {
-      message = 'Select two files to detect sample name.';
+    if(!(this.get('pair.isFull'))) {
+      message = `Select ${this.get('pair.size')} files to detect sample name.`;
     } else if(Ember.isEmpty(name)) {
       // If files are chosen, indicate the automatic naming failed
       message = 'No sample name detected. File names must begin with a common name to detect sample name.';
