a5879151a64448f1297c8a5a976ac2c9a2eb22eb
Dan Leehr <dan.leehr@duke.edu>
2018-01-29 10:56:39 -0500

    Updates FASTQFileItemList
    - All existing tests passing

--- tests/unit/utils/fastq-file-item-list-test.js
+++ tests/unit/utils/fastq-file-item-list-test.js
@@ -17,18 +17,20 @@ function makeMockFileItem(name) {
   });
 }
 
-test('it calculates fastqFilePairs', function(assert) {
+test('it calculates ddsFiles', function(assert) {
   let fileItemList = FASTQFileItemList.create();
   fileItemList.addFileItem(makeMockFileItem('abc_1'));
   fileItemList.addFileItem(makeMockFileItem('abc_2'));
   fileItemList.addFileItem(makeMockFileItem('def_1'));
   fileItemList.addFileItem(makeMockFileItem('def_2'));
-  const expectedPairs = [
-    {name: 'abc', file1: {name: 'abc_1', kind: 'dds-file'}, file2: {name: 'abc_2', kind: 'dds-file'}},
-    {name: 'def', file1: {name: 'def_1', kind: 'dds-file'}, file2: {name: 'def_2', kind: 'dds-file'}}
+  const expectedDdsFiles = [
+    {name: 'abc_1', kind: 'dds-file'},
+    {name: 'abc_2', kind: 'dds-file'},
+    {name: 'def_1', kind: 'dds-file'},
+    {name: 'def_2', kind: 'dds-file'}
     ];
-  const calculatedPairs = fileItemList.get('fastqFilePairs');
-  assert.equal(JSON.stringify(calculatedPairs), JSON.stringify(expectedPairs))
+  const calculatedDdsFiles = fileItemList.get('ddsFiles');
+  assert.equal(JSON.stringify(calculatedDdsFiles), JSON.stringify(expectedDdsFiles ))
 });
 
 test('it calculates cwlObjectValue', function(assert) {
@@ -55,34 +57,34 @@ test('it extracts sample names with delimiter', function(assert) {
 test('It recalculates sample name as files are added', function(assert) {
   let fileItemList = FASTQFileItemList.create();
   fileItemList.addFileItem(makeMockFileItem('AB1234_L001_R1.fastq'));
-  assert.equal(fileItemList.get('fastqFilePairs')[0].get('name'), ''); // initially no common prefix since only one file
+  assert.notOk(fileItemList.get('samples')[0].get('sampleName')); // initially no common prefix since only one file
   fileItemList.addFileItem(makeMockFileItem('AB1234_L001_R2.fastq'));
-  assert.equal(fileItemList.get('fastqFilePairs')[0].get('name'), 'AB1234');
+  assert.equal(fileItemList.get('samples')[0].get('sampleName'), 'AB1234');
 });
 
 test('It sets empty sample name when no match before delimiter', function(assert) {
   let fileItemList = FASTQFileItemList.create();
   fileItemList.addFileItem(makeMockFileItem('SA04059_S3_L007_R2_001.fastq.gz'));
   fileItemList.addFileItem(makeMockFileItem('SA03505_S1_L007_R2_001.fastq.gz'));
-  assert.equal(fileItemList.get('fastqFilePairs')[0].get('name'), '');
+  assert.notOk(fileItemList.get('samples')[0].get('sampleName'));
 });
 
 test('it calculates isComplete', function(assert) {
   let fileItemList = FASTQFileItemList.create();
   Ember.run(() => {
     // First, check an empty list
-    assert.equal(fileItemList.get('fastqFilePairs.length'), 0);
+    assert.equal(fileItemList.get('samples.length'), 0);
     assert.ok(fileItemList.get('isComplete')); // Empty list should be complete
 
     // Now, add a single item. list should not be complete since group size is 2
     fileItemList.addFileItem(makeMockFileItem('abc1'));
-    assert.equal(fileItemList.get('groupSize'), 2); // 2 = pair
-    assert.equal(fileItemList.get('fastqFilePairs.length'), 1); // 1 pair
+    assert.equal(fileItemList.get('filesPerSample'), 2); // 2 = pair
+    assert.equal(fileItemList.get('samples.length'), 1); // 1 pair
     assert.notOk(fileItemList.get('isComplete')); // not complete
 
     // Add a second item, list should be complete
     fileItemList.addFileItem(makeMockFileItem('def2'));
-    assert.equal(fileItemList.get('fastqFilePairs.length'), 1); // still 1 pair
+    assert.equal(fileItemList.get('samples.length'), 1); // still 1 pair
     assert.ok(fileItemList.get('isComplete')); // complete
 
     // Remove the item, list should again be incomplete
@@ -101,7 +103,7 @@ test('it calculates hasUniqueSampleNames', function(assert) {
   let fileItemList = FASTQFileItemList.create();
   Ember.run(() => {
     // First, check an empty list
-    assert.equal(fileItemList.get('fastqFilePairs.length'), 0);
+    assert.equal(fileItemList.get('samples.length'), 0);
     assert.ok(fileItemList.get('hasUniqueSampleNames')); // Empty list should count as unique names
 
     // Add two files to make one sample
