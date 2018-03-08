86892e2703a96a3a397b8dae69611979690bb513
Dan Leehr <dan.leehr@duke.edu>
2018-03-08 11:09:27 -0500

    Test that answerable-field renders nothing, since it is just a base component

--- tests/integration/components/questionnaire/answerable-field-test.js
+++ tests/integration/components/questionnaire/answerable-field-test.js
@@ -5,21 +5,7 @@ moduleForComponent('questionnaire/answerable-field', 'Integration | Component | 
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test('it renders nothing', function(assert) {
   this.render(hbs`{{questionnaire/answerable-field}}`);
-
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#questionnaire/answerable-field}}
-      template block text
-    {{/questionnaire/answerable-field}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
