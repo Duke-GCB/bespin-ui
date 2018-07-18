46ea2367fc8d9f7d3d383cd6d194b6aecddf11f1
Dan Leehr <dan.leehr@duke.edu>
2018-07-18 14:30:35 -0400

    Remove app/session-stores/application.js
    Fixes #125

--- app/session-stores/application.js
+++
@@ -1,7 +0,0 @@
-// app/session-stores/application.js
-import CookieStore from 'ember-simple-auth/session-stores/cookie';
-
-export default CookieStore.extend({
-  cookieExpirationTime: 7200,
-  cookieName: 'bespinsession'
-});
