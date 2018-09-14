e5564e17c7596cfe212830b697f230a00963ef00
Dan Leehr <dan.leehr@duke.edu>
2018-09-14 14:03:30 -0400

    Update FAQ questions to clarify audience and GAAB role

--- app/components/faqs/faq-list.js
+++ app/components/faqs/faq-list.js
@@ -9,18 +9,28 @@ export default Ember.Component.extend({
         'Bespin is a web-based platform that makes it easy to run computational genomics workflows (pipelines) in the cloud.' +
         '\n\n' +
         'A service of the [Computational Solutions Shared Resource](https://genome.duke.edu/cores/computing) ' +
-        'in [Duke GCB](https://genome.duke.edu), Bespin provides best-practice workflows curated by experts. ' +
-        'Specifically, Bespin\'s workflows are built using reproducible software environments, run in GCB\'s private, on-premises cloud, ' +
-        'and although selecting, configuring and running the workflows are self-service, results are reviewed by experts.' +
+        'in [Duke GCB](https://genome.duke.edu), Bespin incorporates best-practice workflows curated by experts. ' +
+        'Specifically, Bespin\'s workflows are built using containerized software environments, run in GCB\'s private, on-premises cloud, ' +
+        'and used by bioinformaticians in the [Genomic Analysis and Bioinformatics](https://genome.duke.edu/cores-and-services/genomic-analysis-and-bioinformatics) ' +
+        'core to provide highly reproducible, expert-reviewed computational results.'
+    },
+    {
+      question: 'Can I use Bespin to analyze my data?',
+      answer:
+        'Bespin is not available to users outside of the GCB core facilities. Bespin is primarily operated by the ' +
+        '[Genomic Analysis and Bioinformatics](https://genome.duke.edu/cores-and-services/genomic-analysis-and-bioinformatics) ' +
+        'core in an effort to streamline popular workflows and encourage best practices for reproducibility ' +
+        'and portability.' +
         '\n\n' +
-        'Bespin is currently available in **beta** to researchers across Duke.'
+        'While Bespin is not generally available, Bespin and its workflows are open source, available at ' +
+        '[github.com/Duke-GCB/bespin](https://github.com/Duke-GCB/bespin).'
     },
     {
       question: 'What kinds of workflows can Bespin run?',
       answer:
         'Bespin has been designed to run any kind of computational genomic workflow where most of the steps can be automated. ' +
         '\n\n' +
-        'Initially, we are offering a Whole Exome Sequencing workflow using [GATK](https://software.broadinstitute.org/gatk/). ' +
+        'Initially, we are targeting a Whole Exome Sequencing workflow using [GATK](https://software.broadinstitute.org/gatk/). ' +
         'We are actively working on additional workflows, and plan to bring them online in the near future.' +
         '\n\n' +
         'Please see the [workflows](/workflows) section for full details on available offerings. ' +
@@ -30,8 +40,8 @@ export default Ember.Component.extend({
     {
       question: 'How long does it take to run a workflow, and how much will it cost?',
       answer:
-        'The computational resources used by Bespin jobs will be billed based on how long they take and how many CPU cores they use.' +
-        'How longa workflow runs depends mostly on the input data size and resources (CPU, memory) assigned to the workflow.' +
+        'The computational resources used by Bespin jobs will be billed based on how long they take and how many CPU cores they use. ' +
+        'How long a workflow runs depends mostly on the input data size and resources (CPU, memory) assigned to the workflow.' +
         '\n\n' +
         'While a workflow is in the **beta** phase, there will be no charge to run it on Bespin. We want to get an idea of what ' +
         'kinds of jobs are working and what we need to improve. We\'ll also get an idea of how long jobs will take and what ' +
@@ -43,7 +53,9 @@ export default Ember.Component.extend({
         'corresponding to a computational resource cost of about $90. ' +
         '\n\n' +
         'Note that this includes _only_ the cost for utilizing the computational resources (CPU, memory, storage). ' +
-        'Additional costs for consultation and review by the Genomic Analysis and Bioinformatics core will apply in addition.'
+        'Additional costs for consultation and review by the ' +
+        '[Genomic Analysis and Bioinformatics](https://genome.duke.edu/cores-and-services/genomic-analysis-and-bioinformatics) ' +
+        'core will apply in addition.'
     },
     {
       question: 'How do I use my data in Bespin?',
@@ -57,13 +69,6 @@ export default Ember.Component.extend({
         'Yes. Each job in Bespin has a limit of 10TB of input data. Beyond this point, another platform ' +
         '(e.g. an HPC cluster such as [HARDAC](https://genome.duke.edu/cores-and-services/computational-solutions/compute-environments-genomics)) ' +
         'is likely a better fit for your project.'
-    },
-    {
-      question: 'What is an authorization code and how do I get one?',
-      answer:
-        'Some workflows require an authorization code before you can start a job. ' +
-        'These codes are provided by the workflow\'s expert reviewers, who will be ' +
-        'able to determine if the workflow is a good fit for your dataset.'
     },
     {
       question: 'Is Bespin approved for datasets containing Protected Health Information (PHI) or other sensitive data?',
