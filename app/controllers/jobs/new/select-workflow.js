38c07ff72799d304f1155c62a9c43c03a0446218
John Bradley <johnbradley2008@gmail.com>
2019-03-08 13:29:15 -0500

    fix for select workflow next button

--- app/controllers/jobs/new/select-workflow.js
+++ app/controllers/jobs/new/select-workflow.js
@@ -10,7 +10,7 @@ export default Ember.Controller.extend({
       this.set('workflow', workflow);
     },
     next() {
-      let workflowVersionId = this.get('workflow.latestEnabledVersion.id');
+      let workflowVersionId = this.get('workflow.latestVersion.id');
       if (workflowVersionId) {
         this.transitionToRoute('jobs.new.select-questionnaire', workflowVersionId);
       }
