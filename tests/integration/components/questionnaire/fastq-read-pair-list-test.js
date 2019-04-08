871cfa11088be1908b9f433742f04f9fc4f26126
Dan Leehr <dan.leehr@duke.edu>
2019-04-08 11:09:00 -0400

    set layoutName to render the parent's layout
    - Noted in https://github.com/emberjs/rfcs/issues/412#issuecomment-356975507 that layoutName is private API, but tests will detect if this breaks

--- tests/integration/components/questionnaire/fastq-read-pair-list-test.js
+++ tests/integration/components/questionnaire/fastq-read-pair-list-test.js
@@ -1,10 +1,24 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import StoreStub from '../../../helpers/store-stub';
+import Ember from 'ember';
 
 moduleForComponent('questionnaire/fastq-read-pair-list', 'Integration | Component | questionnaire/fastq read pair list', {
-  integration: true
+  integration: true,
+  beforeEach: function() {
+    this.register('service:store', StoreStub);
+  }
 });
 
-test('it renders', function(assert) {
-  assert.ok(true);
+test('it renders using the fastq-file-pair-list template', function(assert) {
+  const fileItems = Ember.Object.create({
+      samples: [{},{}]
+    }
+  );
+  this.set('fileItems', fileItems);
+  this.set('externalAction', () => {});
+  this.render(hbs`{{questionnaire/fastq-read-pair-list "FieldName" (action externalAction) fileItems=fileItems}}`);
+  assert.equal(this.$('.fastq-file-pair-row').length, 2);
+  assert.equal(this.$('div.file-group-list-picker').length, 1);
+
 });
