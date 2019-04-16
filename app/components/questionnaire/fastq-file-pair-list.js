58cb375a9c0dca47ca017589b59fe25ef6855c3b
Dan Leehr <dan.leehr@duke.edu>
2019-04-16 16:37:52 -0400

    Consistency in import

--- app/components/questionnaire/fastq-file-pair-list.js
+++ app/components/questionnaire/fastq-file-pair-list.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import FileGroupList from 'bespin-ui/components/questionnaire/file-group-list';
 import { FASTQFileItemList } from 'bespin-ui/utils/fastq-file-item-list';
-import layout from '../../templates/components/questionnaire/fastq-file-pair-list';
+import layout from 'bespin-ui/templates/components/questionnaire/fastq-file-pair-list';
 
 const FASTQFilePairList = FileGroupList.extend({
   layout, // https://github.com/emberjs/rfcs/issues/412#issuecomment-423499140
