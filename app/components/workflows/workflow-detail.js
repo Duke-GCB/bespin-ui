6411b3e33ed786741d37d30a636c8908a86fa76f
Dan Leehr <dan.leehr@duke.edu>
2019-05-16 10:42:47 -0400

    Update and add new tests for workflow-detail
    - Decorates html elements with classes for easier testing
    - workflow-detail now renders a table with links/descriptions of versions

--- app/components/workflows/workflow-detail.js
+++ app/components/workflows/workflow-detail.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const WorkflowDetail = Ember.Component.extend({
+  classNames: ['workflow-detail'],
   workflow: null,
-  onlyShowCurrent: false,
-  sortedVersions: Ember.computed('onlyShowCurrent', 'workflow.versions[]', function() {
+  sortedVersionsNewestFirst: Ember.computed('workflow.versions[]', function() {
     const versions = this.get('workflow.versions').sortBy('version').reverse();
-    if (this.get('onlyShowCurrent')) {
-      return versions.slice(0, 1);
-    }
     return versions;
   })
 });
+
+WorkflowDetail.reopenClass({
+  positionalParams: ['workflow']
+});
+
+export default WorkflowDetail;
--- tests/integration/components/workflows/workflow-detail-test.js
+++ tests/integration/components/workflows/workflow-detail-test.js
@@ -1,44 +1,64 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
+const workflowVersion2 = {
+  id: '2',
+  version: 'v2.0.0',
+  description: 'My workflow2',
+  created: 'Feb 1 2017',
+  url: 'http://example.org/v2.cwl'
+};
+
+const workflowVersion3 = {
+  id: '3',
+  version: 'v3.0.0',
+  description: 'My workflow3',
+  created: 'Feb 13 2017',
+  url: 'http://example.org/v3.cwl'
+};
+
+const workflow ={
+  id: '1',
+  name: 'Exomeseq',
+  versions: [workflowVersion2, workflowVersion3]
+};
+
 moduleForComponent('workflows/workflow-detail', 'Integration | Component | workflows/workflow detail', {
-  integration: true
+  integration: true,
+  setup() {
+    this.container.lookup('router:main').setupRouter();
+  },
+  beforeEach() {
+    this.set('workflow', workflow);
+  }
 });
 
-test('it renders', function(assert) {
-  const workflowVersion2 = {
-    version: '2',
-    description: 'My workflow2',
-    created: 'Feb 1 2017',
-    url: 'somewhere.edu'
-  };
-  const workflowVersion3 = {
-    version: '3',
-    description: 'My workflow3',
-    created: 'Feb 13 2017',
-    url: 'somewhere.edu'
-  };
-  this.set('myworkflow', {
-      name: 'Exomeseq',
-      versions: [workflowVersion2, workflowVersion3]
-  });
+test('it renders one element with class workflow-detail', function(assert) {
+  this.render(hbs`{{workflows/workflow-detail workflow}}`);
+  assert.equal(this.$('.workflow-detail').length, 1);
+});
 
-  // render all versions
-  this.render(hbs`{{workflows/workflow-detail workflow=myworkflow}}`);
-  assert.equal(this.$('h3').text().trim().replace(/ /g,''), 'Workflow:\nExomeseq');
-  //highest version should be sorted first
-  assert.equal(this.$('.workflow-version-detail-markdown p').text(), 'My workflow3' + 'My workflow2');
+test('it renders the workflow name', function(assert) {
+  this.render(hbs`{{workflows/workflow-detail workflow}}`);
+  assert.equal(this.$('.workflow-detail .name').text(), 'Exomeseq');
+  assert.equal(this.$('.workflow-detail .name').length, 1);
+});
 
-  // render only current version
-  this.render(hbs`{{workflows/workflow-detail workflow=myworkflow onlyShowCurrent=true}}`);
-  assert.equal(this.$('.workflow-version-detail-markdown p').text(), 'My workflow3');
+test('it renders a versions table with a header row', function(assert) {
+  this.render(hbs`{{workflows/workflow-detail workflow}}`);
+  assert.equal(this.$('.workflow-detail table.versions-table').length, 1);
+  assert.equal(this.$('.workflow-detail table.versions-table tr.header-row').length, 1);
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#workflows/workflow-detail workflow=myworkflow}}
-      <span class="childcontent">template block text</span>
-    {{/workflows/workflow-detail}}
-  `);
+test('it renders a row for each workflow version', function(assert) {
+  this.render(hbs`{{workflows/workflow-detail workflow}}`);
+  assert.equal(this.$('.workflow-detail table.versions-table tr.version-row').length, 2);
+});
 
-  assert.equal(this.$('.childcontent').html().trim(), 'template block text');
+test('it renders link, description, and created', function(assert) {
+  this.render(hbs`{{workflows/workflow-detail workflow}}`);
+  assert.equal(this.$('tr.version-row td.version-cell a').last().attr('href'), '/workflows/1/versions/2');
+  assert.equal(this.$('tr.version-row td.version-cell a').last().text().trim(), 'v2.0.0');
+  assert.equal(this.$('tr.version-row td.description-cell').last().text().trim(), 'My workflow2');
+  assert.equal(this.$('tr.version-row td.created-cell').last().text().trim(), 'February 1, 2017');
 });
