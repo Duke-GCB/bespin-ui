981f4a477cc9e339af75dd67481292edf547eb1d
Dan Leehr <dan.leehr@duke.edu>
2019-05-16 11:40:09 -0400

    Tests for /workflow-version-summary

--- tests/integration/components/workflows/workflow-version-history-test.js
+++ tests/integration/components/workflows/workflow-version-history-test.js
@@ -22,14 +22,14 @@ test('it renders', function(assert) {
   assert.equal(this.$('.workflow-version-history').length, 1);
 });
 
-test('it renders panel with tag and info url', function(assert) {
+test('it renders panel heading with tag and info url', function(assert) {
   this.render(hbs`{{workflows/workflow-version-history workflowVersion}}`);
   assert.equal(this.$('.workflow-version-history span.lead .history-tag').text(), 'Version History: wf/version');
   assert.equal(this.$('.workflow-version-history span.lead .info-link a').text(), 'Version Info');
   assert.equal(this.$('.workflow-version-history span.lead .info-link a').attr('href'), 'http://version.info');
 });
 
-test('it renders markdown from getVersionInfo into HTML', function(assert) {
+test('it renders panel body with markdown from getVersionInfo into HTML', function(assert) {
   this.render(hbs`{{workflows/workflow-version-history workflowVersion}}`);
   // getVersionInfo returns '# Version Info', so look for <h1>Version Info</h1>
   const renderedMarkdown = this.$('.workflow-version-history .panel-body h1');
--- tests/integration/components/workflows/workflow-version-summary-test.js
+++ tests/integration/components/workflows/workflow-version-summary-test.js
@@ -1,25 +1,45 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+const workflowVersion =  Ember.Object.create({
+  versionTag: 'wf/version',
+  url: 'http://version.download',
+  description: 'version xyz',
+  created: 'Jan 1, 2019'
+});
+
 
 moduleForComponent('workflows/workflow-version-summary', 'Integration | Component | workflows/workflow version summary', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.set('workflowVersion', workflowVersion);
+  }
 });
 
 test('it renders', function(assert) {
+  this.render(hbs`{{workflows/workflow-version-summary workflowVersion}}`);
+  assert.equal(this.$('.workflow-version-summary').length, 1);
+});
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders panel heading with tag and download url', function(assert) {
+  this.render(hbs`{{workflows/workflow-version-summary workflowVersion}}`);
+  assert.equal(this.$('.workflow-version-summary span.lead .summary-tag').text(), 'Version Summary: wf/version');
+  assert.equal(this.$('.workflow-version-summary span.lead .download-link a').text(), 'Download Workflow');
+  assert.equal(this.$('.workflow-version-summary span.lead .download-link a').attr('href'), 'http://version.download');
+});
 
-  this.render(hbs`{{workflows/workflow-version-summary}}`);
+test('it renders panel body with description and release date', function(assert) {
+  this.render(hbs`{{workflows/workflow-version-summary workflowVersion}}`);
+  assert.equal(this.$('.workflow-version-summary .panel-body .description').text(), 'Description: version xyz');
+  assert.equal(this.$('.workflow-version-summary .panel-body .released').text(), 'Released: January 1, 2019');
+});
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+test('it yields inside panel body', function(assert) {
   this.render(hbs`
-    {{#workflows/workflow-version-summary}}
-      template block text
+    {{#workflows/workflow-version-summary workflowVersion}}
+      <span class="hello">Hi</span>
     {{/workflows/workflow-version-summary}}
   `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.workflow-version-summary .panel-body span.hello').text(), 'Hi');
 });
