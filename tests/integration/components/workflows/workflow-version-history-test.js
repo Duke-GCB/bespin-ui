d791da28ee440f745f001fb90c133828ab6c95bc
Dan Leehr <dan.leehr@duke.edu>
2019-05-16 11:26:59 -0400

    Tests for workflow-version-history

--- tests/integration/components/workflows/workflow-version-history-test.js
+++ tests/integration/components/workflows/workflow-version-history-test.js
@@ -1,25 +1,37 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 
+const workflowVersion =  Ember.Object.create({
+  getVersionInfo() {
+    return Ember.RSVP.resolve({content: btoa('# Version Info')})
+  },
+  versionTag: 'wf/version',
+  versionInfoUrl: 'http://version.info'
+});
+
 moduleForComponent('workflows/workflow-version-history', 'Integration | Component | workflows/workflow version history', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.set('workflowVersion', workflowVersion);
+  }
 });
 
 test('it renders', function(assert) {
+  this.render(hbs`{{workflows/workflow-version-history workflowVersion}}`);
+  assert.equal(this.$('.workflow-version-history').length, 1);
+});
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders panel with tag and info url', function(assert) {
+  this.render(hbs`{{workflows/workflow-version-history workflowVersion}}`);
+  assert.equal(this.$('.workflow-version-history span.lead .history-tag').text(), 'Version History: wf/version');
+  assert.equal(this.$('.workflow-version-history span.lead .info-link a').text(), 'Version Info');
+  assert.equal(this.$('.workflow-version-history span.lead .info-link a').attr('href'), 'http://version.info');
+});
 
-  this.render(hbs`{{workflows/workflow-version-history}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#workflows/workflow-version-history}}
-      template block text
-    {{/workflows/workflow-version-history}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it renders markdown from getVersionInfo into HTML', function(assert) {
+  this.render(hbs`{{workflows/workflow-version-history workflowVersion}}`);
+  // getVersionInfo returns '# Version Info', so look for <h1>Version Info</h1>
+  const renderedMarkdown = this.$('.workflow-version-history .panel-body h1');
+  assert.equal(renderedMarkdown.text(), 'Version Info');
 });
