f87bbacdb188a393aed798efd459fa6fd2513322
Dan Leehr <dan.leehr@duke.edu>
2019-05-17 09:50:20 -0400

    Implements versionSort and tests
    Based on https://github.com/Duke-GCB/bespin-api/blob/112ae41d419f344e3d6b5c0f4d775bf070a30d4c/data/models.py

--- app/utils/version-sort.js
+++ app/utils/version-sort.js
@@ -1,3 +1,20 @@
-export default function versionSort(versionStrings) {
-  return versionStrings;
+// Version sorting adapted from python implementation in
+// https://github.com/Duke-GCB/bespin-api/blob/112ae41d419f344e3d6b5c0f4d775bf070a30d4c/data/models.py
+
+const PartSortDigits = 10;
+
+function makeSortKey(versionString) {
+  const parts = [];
+  versionString.split(/\.|\-/).forEach( part => {
+    let leftPaddedPart = part.padStart(PartSortDigits, '0');
+    parts.push(leftPaddedPart);
+  });
+  return parts.join('');
+}
+
+export default function versionSort(versions) {
+  const versionsAndKeys = versions.map(version => {
+    return {version: version, key: makeSortKey(version)};
+  });
+  return versionsAndKeys.sortBy('key').mapBy('version');
 }
--- tests/unit/utils/version-sort-test.js
+++ tests/unit/utils/version-sort-test.js
@@ -3,9 +3,20 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | version sort');
 
-// Replace this with your real tests.
-test('it sorts version strings', function(assert) {
-  const unsorted = ['3.0','1.0','2.1.1','2.0'];
+test('it sorts simple, single-digit versions', function(assert) {
+  const unsorted = ['2','4','5','1','3'];
   let sorted = versionSort(unsorted);
-  assert.equal(sorted, ['1.0','2.0','2.1.1','3.0']);
+  assert.deepEqual(sorted, ['1','2','3','4','5']);
+});
+
+test('it sorts semantic versions with differing numbers of components', function(assert) {
+  const unsorted = ['3.0.0','2.2','1.0','2.1.1','2'];
+  let sorted = versionSort(unsorted);
+  assert.deepEqual(sorted, ['1.0','2','2.1.1','2.2', '3.0.0']);
+});
+
+test('it sorts versions with extra non-numeric characters', function(assert) {
+  const unsorted = ['1','2.2.2-dev','1.3.1','5'];
+  let sorted = versionSort(unsorted);
+  assert.deepEqual(sorted, ['1','1.3.1','2.2.2-dev','5']);
 });
