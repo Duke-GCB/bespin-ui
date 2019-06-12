83efd0c8ed1bd0c18d23038322e938616c345417
John Bradley <johnbradley2008@gmail.com>
2019-06-12 10:45:18 -0400

    remove tests moved to active-workflows-tests

--- tests/unit/routes/jobs/new/select-workflow-test.js
+++ tests/unit/routes/jobs/new/select-workflow-test.js
@@ -8,24 +8,3 @@ test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
 });
-
-test('it sets model to all active workflows', function(assert) {
-  let route = this.subject({
-    store: {
-      findAll(recordModel) {
-        return {
-          then(filterFunc) {
-            return filterFunc([
-              Ember.Object.create({id: 1, kind: 'find_' + recordModel, isActive: true}),
-              Ember.Object.create({id: 2, kind: 'find_' + recordModel, isActive: false}),
-            ])
-          }
-        };
-      }
-    }
-  });
-  let model = route.model();
-  assert.equal(model.length, 1);
-  assert.equal(model[0].get('kind'), 'find_workflow');
-  assert.equal(model[0].get('id'), 1);
-});
