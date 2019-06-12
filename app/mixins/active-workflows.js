8a0e3b347b8c7e979d153b95e20aff3fbd5631bf
John Bradley <johnbradley2008@gmail.com>
2019-06-12 11:13:17 -0400

    add comment about reload

--- app/mixins/active-workflows.js
+++ app/mixins/active-workflows.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   model(){
+    // reload is set to true to skip cached workflows to handle the scenario where the cache is not fully populated
+    // with all active workflows.
     return this.get('store').findAll('workflow', { reload: true }).then(
       workflows => workflows.filterBy('isActive').sortBy('name')
     );
